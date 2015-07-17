e8317dda0432ef78c9a80b1c05c5a4ba7862bcbb
Cheikh Ndiongue <Cmndiongue@yahoo.com>
2015-07-17 15:59:12 -0400

    added solution

--- high_card.rb
+++ high_card.rb
@@ -2,4 +2,31 @@
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
-players = []
+deck = []
+
+ranks.each do |rank|
+    suits.each do |suit|
+        card = [rank, suit]
+        deck.push (card)
+    end
+end
+   puts deck.inspect
+
+
+puts "Please enter your name?"
+name = gets.chomp
+
+puts "play"
+player = gets.chomp
+
+until player == "play" do
+	puts()
+	
+end
+
+
+
+def 
+
+
+
