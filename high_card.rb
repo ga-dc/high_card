758d2300e2b099d141c2c4787c654a7ff8901478
Silvia Brown <siltastic@gmail.com>
2015-07-16 23:07:08 -0400

    Added solution. Finding highest card and winner needs improvement.

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,44 @@
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
+deck = []
 
+# Create a deck of cards.
+ranks.each do |rank|
+  suits.each do |suit|
+    # Create mini arrays.
+    mini_deck = [rank, suit]
+    deck.push(mini_deck)
+  end  
+end
+
+# Shuffle the deck of cards.
+puts deck.shuffle.inspect
+
+# Place all players name in an array.
 players = []
+
+# Ask the player's name.
+puts "#{players.length} players so far. Enter a player name, or type 'play':"
+
+# Receive th player's name.
+name = gets.chomp.to_s
+
+# Ask the player for a name or to play. Repeatedly!
+# Name doesn't know "play" yet.
+while (name != "play") # Will break the loop when "play" is typed.
+  players.push(name) # Push the name into the players variable.
+  puts "#{players.length} players so far. Enter a player name, or type 'play':"
+  name = gets.chomp.to_s
+end
+
+# Reshuffle the deck.
+dealed_card = deck.shuffle.pop(players.length)
+puts dealed_card
+
+# Find the wining card.
+# winning_card = dealed_card.index(dealed_card.max)
+
+# Find the winner.
+# winning_player = players[winning_card.to_i]
+# puts "The winner is #{winning_player}"
