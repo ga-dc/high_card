24bd6052a6b5938c399fa7f730bdba13fc783161
Omari Mayers-Walker <omari@fastmail.fm>
2015-07-16 23:20:32 -0400

    created card deck shuffle and player input

--- high_card.rb
+++ high_card.rb
@@ -3,9 +3,24 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+winners = []
 
-# .product; .inspect
-
+# Shuffle Deck
 deck = ranks.product(suits)
-deck.shuffle
-puts deck.inspect
+shuffled_deck = Array(deck.inspect)
+shuffledCards = shuffled_deck.shuffle
+
+# Get Names
+while true
+   puts "#{players.length} players so far. Enter a player name, or type 'play':"
+
+   name = gets.chomp
+
+   break if name == "play"
+   players.push(name)
+end
+
+delt_cards = players.map do |player| 
+   shuffled_deck.pop
+end
+
