21e540b8d458e9348a36618024d50e5fb4faccf9
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-07-16 21:42:38 -0400

    added comments

--- high_card.rb
+++ high_card.rb
@@ -1,4 +1,5 @@
-# Use these two arrays to generate a deck of cards.
+#moved ace to end of rank array
+#nested .each combines rank and suit in the deck array
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10,"J", "Q", "K", "A"]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
@@ -8,9 +9,11 @@ ranks.each do |rank|
     deck.push([rank, suit])
   end
 end
-
 deck.shuffle!
 
+#create playerName variable with length 0 and empty players array
+#add names to players array until play is entered
+#break the loop before play is entered into the players array
 players = []
 playerName = ""
 until playerName == "play" do
@@ -22,14 +25,20 @@ until playerName == "play" do
   players.push(playerName)
 end
 
+#for each player, pull a rank from the deck and push it into a playerCards array
 playerCards = []
-
 (players.length).times do |i|
   cardRank = deck[i][0]
   playerCards.push(cardRank)
 end
 puts playerCards
 
+#googled how to get the index of an element from an array (in js this would be indexOf)
+#created a hash that includes each rank (2 through A) with it's index in the rank array
+#created a playerRanks array that takes the indexes of the rank of each card dealt to the players
+#get the index of the max value in the ranks array
+#the winning player will have that same index in the playerNames array
+#this will only return one max value (and therefore name) even if more than one player had the highest rank
 playerRanks = []
 hashRanks = Hash[ranks.map.with_index.to_a]
 #puts hashRanks
