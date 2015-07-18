183a3b0a09176fdfa94bae6d102b564e62470275
Stephen Gheysens <segheysens@gmail.com>
2015-07-18 11:34:34 -0400

    Implemented player array and took input to create the array

--- high_card.rb
+++ high_card.rb
@@ -14,4 +14,22 @@ end
 
 deck.shuffle!
 
-puts deck
+input = ""
+names = []
+
+until input == "play"
+  puts "#{names.length} players so far. Enter a player name, or type 'play':"
+  input = gets.chomp
+  if input != "play"
+    names.push(input)
+  end
+end
+
+puts names
+#
+# 2. Collect an array of player names by prompting:
+# 	* **"{n} players so far. Enter a player name, or type 'play':"**
+# 3. Upon *"play"*, deal each player a card.
+# 4. Find the highest card score dealt (Aces high).
+# 5. Find the winning player name, then print out:
+# 	* **"Winner(s): {name1, name2, …}!"**
