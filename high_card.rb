e6f0694cd213fd78973663136bf2d135426dc5ca
Jeff Jones <jeff.jones1@gmail.com>
2015-07-17 20:06:01 -0400

    functionality up to hands

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,27 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
-
+deck =[]
 players = []
+
+
+ranks.each do |index|
+  suits.each do |suit|
+    deck.push([index,suit])
+    deck.shuffle!
+  end
+end
+
+hands = {}
+new_player = ''
+until new_player == 'play' do
+  if (new_player != 'play')
+  puts players.length.to_s + " players so far. Enter a player name, or type 'play':"
+new_player = gets.chomp.to_s.downcase
+if (new_player == 'play')
+  break
+end
+  players.push(new_player)
+  hands[:"#{new_player}"] = deck.pop
+end
+end
