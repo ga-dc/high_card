4e3a083b8ce8835da294ed1987f01109f2eef715
Stephen Gheysens <segheysens@gmail.com>
2015-07-18 11:17:22 -0400

    Created deck and shuffled deck

--- high_card.rb
+++ high_card.rb
@@ -3,3 +3,15 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+
+deck = []
+
+for rank in ranks
+  for suit in suits
+    deck.push([rank, suit])
+  end
+end
+
+deck.shuffle!
+
+puts deck
