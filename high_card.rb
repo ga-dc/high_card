c5fccb525d11a5755b4f05ce8022697511eebb07
Cameron Fry <cameronfry11@gmail.com>
2015-07-19 13:24:15 -0400

    Added functionality to create a shuffled deck from ranks and suits arrays. Also began work on receiving user input to add players to player's array and calling play function on play

--- high_card.rb
+++ high_card.rb
@@ -3,3 +3,39 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+
+#Use a method to assign each suit to a type of card
+
+#Use shuffle method to shufle the deck
+
+#FULL SHUFFLED DECK
+
+rank_per_card = ranks*4
+suits_per_card = suits*13
+
+deck = rank_per_card.zip(suits_per_card)
+
+deck.each_slice(2).to_a
+
+deck = deck.shuffle
+
+#RECEIVE USER INPUT FOR NAME AND CALL PLAY FUNCTION
+
+$play_called = false
+
+until $play_called == true do
+print " #{players.count} players so far. Enter a player name, or type 'play': "
+user_input = gets
+
+  if user_input.downcase == "play"
+    play()
+    $play_called = true
+  else
+    players << user_input
+    puts players
+  end
+end
+
+def play()
+  print "It's time to play"
+end
