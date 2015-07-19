d16999ab1aff9a1d58fe3933eee5ae303e637647
ktw1222 <ktw1222@gmail.com>
2015-07-19 03:52:57 -0400

    build a deck and shuffle

--- high_card.rb
+++ high_card.rb
@@ -3,3 +3,16 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+
+
+# build a deck of cards
+deck = []
+
+ranks.each do |rank|
+  suits.each do |suit|
+    deck.push([rank, suit])
+  end
+end
+
+# shuffle the deck
+deck.shuffle
