36c8f56d4acf74f2b05912c8894a5de57d30a8be
Mostafa Habib <MostafaSHabib@gmail.com>
2015-07-19 19:26:29 -0400

    shuffle and deck working trying to get deal to work

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,38 @@
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
-
+deck = []
 players = []
+hand = []
+game = 1
+
+  ranks.each do |rank|
+      suits.each do |suit|
+        deck.push rank.to_s + suit
+      end
+    end
+
+until game == 2
+
+deck = deck.shuffle
+
+  puts players.length.to_s + " players so far. Enter a player name, or type 'play':"
+  name = gets.chomp.to_s
+
+
+
+  if name == 'play'
+    #GAME STARTS
+    #hand.push deck.sample
+    #players.map { |deal| deck.sample }
+    players.each.push (deck.sample)
+    puts players
+
+    game += 1
+  end
+
+
+  players.push(name)
+
+
+end
