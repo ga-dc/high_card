b070e528bba52ed85d6d5f02e8541d989b9e33ce
Jeremy Koulish <jkoulish@gmail.com>
2015-07-20 01:16:03 -0400

    get and list player names

--- high_card.rb
+++ high_card.rb
@@ -1,3 +1,4 @@
+# initial arrays representing building blocks of cards
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
@@ -8,3 +9,19 @@ ranks.each_with_index do |rank, index|
     cards.push([index, rank, suit])
   end
 end
+
+# prompt for player names
+players = []
+entry = ""
+until (entry == "play")
+  puts ("#{players.length} players so far. Enter a player name, or type 'play':")
+  entry = gets.chomp
+  if gets.chomp != "play"
+    players.push (entry)
+  else entry.clear
+  end
+end
+
+# list players
+players.pop
+puts players
