0cc73ed81748bb8dc8515f5b984f11a3d9523ac4
Jeremy Koulish <jkoulish@gmail.com>
2015-07-20 01:13:47 -0400

    created deck with values for each card

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,10 @@
-# Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
-players = []
+# Use these two arrays to generate a deck of cards.
+cards = []
+ranks.each_with_index do |rank, index|
+  suits.each do |suit|
+    cards.push([index, rank, suit])
+  end
+end
