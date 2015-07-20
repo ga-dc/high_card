63ffb030865cb2a9bb917411f41186f886bbe632
Matt Ficke <matthew.ficke@gmail.com>
2015-07-19 20:42:43 -0400

    bonus #2

--- high_card.rb
+++ high_card.rb
@@ -1,53 +1,78 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
+$ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+$suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 # create hash for values of each card rank
-card_values = Hash.new
-# create arrays for the players and the deck of cards
-players = Array.new
-deck = Array.new
-# array-of-arrays with card rank and suit
-suits.each do |suit|
-  ranks.each_with_index do |rank, index|
-    deck.push([rank, suit])
-    # create hash with card values
-    if rank == "A"
-      card_values[rank] = 14
-    else
-      card_values[rank] = index - 1
+$card_values = Hash.new
+
+def make_deck
+  deck = Array.new
+  # array-of-arrays with card rank and suit
+  $suits.each do |suit|
+    $ranks.each_with_index do |rank, index|
+      deck.push([rank, suit])
+      # create hash with card values
+      if rank == "A"
+        $card_values[rank] = 14
+      else
+        $card_values[rank] = index - 1
+      end
     end
   end
-end
-# shuffle deck in place
-deck.shuffle!
-# gather player names
-input = ""
-while input != 'play'
-  puts "#{players.length} players so far. Enter a player name, or type 'play':"
-  input = gets.chomp
-  if input !='play'
-    players.push(input)
-  end
+  # shuffle deck in place
+  deck.shuffle!
+  return deck
 end
 # create new array-of-arrays with player's name and their card
-counter = 0
-deal = players.map do |player|
-    counter = counter + 1
-    [player, deck[counter]]
+def deal_cards(players)
+  deck = make_deck()
+  counter = 0
+  deal = players.map do |player|
+      counter = counter + 1
+      [player, deck[counter]]
+  end
+  return deal
 end
-# find the highest value card
-high_card =  deal.max_by {|card| card_values[card[1][0]]}
-puts "high card: #{high_card}"
-# select all players whose card is equal in value to the high card
-winners = deal.select{|card| card_values[card[1][0]] == card_values[high_card[1][0]]}
-puts "winners: #{winners}"
-# push the names of the winners to array
-winners_names = []
-winners.each {|name| winners_names.push("#{name[0]}")}
-# check if tie
-if winners_names.length > 1
-  puts "It's a tie between #{winners_names.join(', ')}!"
-else
-  puts "The winner is #{winners_names[0]}!"
+
+def play_game (players)
+  deal = deal_cards(players)
+  high_card =  deal.max_by {|card| $card_values[card[1][0]]}
+  # puts "high card: #{high_card}"
+  # select all players whose card is equal in value to the high card
+  winners = deal.select{|card| $card_values[card[1][0]] == $card_values[high_card[1][0]]}
+  # puts "winners: #{winners}"
+  # push the names of the winners to array
+  winners_names = Array.new
+  winners.each {|name| winners_names.push("#{name[0]}")}
+  end_game(winners_names, players)
 end
+
+def end_game (winners_names, players)
+  # check if tie
+  if winners_names.length > 1
+    puts "Tie! Press 'Enter' to draw again."
+    input = gets
+    if input == "\n"
+      play_game(players)
+    end
+  else
+    puts "The winner is #{winners_names[0]}!"
+  end
+end
+
+def start_game
+  # create arrays for the players and the deck of cards
+  players = Array.new
+  # gather player names
+  input = ""
+  while input != 'play' && players.length < 49
+    puts "#{players.length} players so far. Enter a player name, or type 'play':"
+    input = gets.chomp
+    if input !='play'
+      players.push(input)
+    end
+  end
+  play_game(players)
+end
+
+start_game
