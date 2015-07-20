deac6167e4d22ef8da1684b01414e16ddb222780
Paul Doulatshahi <paul.doulatshahi@gmail.com>
2015-07-19 21:27:19 -0400

    homework

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,23 @@
-# Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
+def shuffledeck
+	ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+	suits = [ "hearts", "spades", "clubs", "diamonds" ]
+	deck = []
+	ranks.each_with_index {|rank| suits.each {|suit| deck.push([rank, suit])}}
+	return deck.shuffle
+end
 
+deck = shuffledeck
 players = []
+
+while true
+	puts "#{players.count} players so far. Enter a player name, or type 'play':"
+	name = gets.chomp
+	break if name == 'play'
+	players.push(name)
+end
+
+hands = []
+
+for person in players
+	hands.push(deck.pop.unshift(person))
+end
