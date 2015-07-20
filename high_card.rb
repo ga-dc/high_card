ea56c1fcc824290e5a6e3cbe37a213b0cf6df66b
Anand Moses <moses.adg@gmail.com>
2015-07-20 06:24:05 -0400

    Deck shuffle

--- high_card.rb
+++ high_card.rb
@@ -3,3 +3,21 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+
+values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+  suits = ['hearts', 'diamonds', 'clubs', 'spades']
+  deck = []
+
+  values.each_with_index do |v, i|
+    suits.each do |s|
+      deck.push({
+        score: i,
+        value: v,
+        suit: s,
+      })
+    end
+
+end
+puts deck
+
+deck.shuffle
