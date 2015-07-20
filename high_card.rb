c28d2f9ddaebfd529cd6435483bba396bc841e0a
Samuel Key <s.key08@gmail.com>
2015-07-20 06:51:31 -0400

    homework

--- high_card.rb
+++ high_card.rb
@@ -3,3 +3,12 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+puts "Player 1 enter your name..."
+players[0] = gets
+puts "Player 2 enter your name..."
+players[1] = gets
+
+player1_card = "#{players[0].to_s + deck.shuffle[0].to_s}"
+puts player1_card
+player2_card = "#{players[1].to_s + deck.shuffle[0].to_s}"
+puts player2_card
