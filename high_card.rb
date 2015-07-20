8de8cd6d3081c76f0fc9962d6536c115d7e53877
Matt Curtis <Curtismn87@yahoo.com>
2015-07-20 08:32:22 -0400

    Got deck into a hash

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,20 @@
-# Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+deck = []
+while players.length < 2
+puts "Player #{players.length + 1}, what is your name?"
+name = gets.chomp
+players.push(name)
+end
+
+
+ranks.each do |rank|
+suits.each do |suit|
+deck.push({
+  rank: rank,
+  card: rank.to_s + " of " + suit
+  })
+end
+end
