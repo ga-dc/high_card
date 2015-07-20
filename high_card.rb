a427776fbe96ceadaae57897f46a054d14571980
Karl Sakas <donusdonut@Karls-MBP.home>
2015-07-20 07:03:39 -0400

    workability minus checking of winner

--- high_card.rb
+++ high_card.rb
@@ -1,28 +1,23 @@
-# Recommended Books by Betsy of Optoro
-# Eloquent Ruby by Russ Olsen
-# Practical Object-Oriented Design in Ruby by Sandi Metz
-# Rails Documentation Guides
-
-deck = []
 players = []
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
-trump = [14, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 ]
 suits = [ "Hearts", "Spades", "Clubs", "Diamonds" ]
 input = nil
 players = []
 hands = []
+deck = []
+check = []
+w = nil
+winner = "hello"
 
-ranks.each do |rank|
+ranks.each_with_index do |rank,index|
   suits.each do |suit|
-    deck.push([rank,suit])
+    deck.push([rank,suit,index])
   end
 end
-
-ranks.each_with_index do |value, index|
-    puts "#{index}: #{value}"
-end
+# end
 
 deck.shuffle!
+puts deck.inspect
 
 while (input != "PLAY") do
   puts "#{players.length} players so far. Enter a player, name or type 'play' to begin:"
@@ -33,29 +28,21 @@ while (input != "PLAY") do
 end
 
       for i in (0..(players.length - 1)) do
-          hands.push([players[i],("#{deck[i][0]} of #{deck[i][1]}")])
+          hands.push([players[i],("#{deck[i][0]} of #{deck[i][1]}"), deck[i][2]])
       end
       puts hands.inspect
 
+      for k in (0..(hands.length - 1)) do
+        check.push(hands[k][2])
+      end
 
-# use max to determine highest index value each player has
+    w = check.max.inspect
 
-# possible solutions: 1. Reformat hands as an array, ensure each with index includes
-#
+    for k in (0..(players.length - 1)) do
+        if hands[k][2] == w
+          winner = hands[k][0]
+        end
+    end
 
-# * `max`
-
-# 4. Find the highest card score dealt (Aces high).
-# 5. Find the winning player name, then print out:
-# 	* **"Winner(s): {name1, name2, …}!"**
-#
-# ### Bonus I
-#
-# Print out *one* of the following outcomes:
-#
-# * **"The winner is {name}!"**
-# * **"It's a tie between {name1, name2, …}!"**
-#
-# ### Bonus II
-#
-# Have the game redraw cards for all players in the case of a tie.
+puts winner
+      # puts "Winner is #{hands[w][0]} with #{hands[w][1]}"
