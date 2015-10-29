8b3570fd0a9a61b7a6598faedec86eb5a1f82ede
Chase Gruber <chasegruber@gmail.com>
2015-10-29 15:34:04 -0400

    build and shuffle deck

--- high_card.rb
+++ high_card.rb
@@ -3,3 +3,16 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+
+deck = []
+
+ranks.length.times do |i|
+  suits.length.times do |b|
+    card = [ranks[i],suits[b]]
+    deck.push(card)
+  end
+end
+
+deck.shuffle!
+
+puts deck.inspect
