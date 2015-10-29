719331141f312ce5dc8069dc1fa8bfce7a11d4dc
James Moon <mooniker@gmail.com>
2015-10-29 17:10:41 -0400

    Create deck (first commit FTW)

--- high_card.rb
+++ high_card.rb
@@ -3,3 +3,16 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+
+deck = []
+
+suits.each do |suit|
+  ranks.each do |rank|
+    deck.push([rank, suit])
+  end
+end
+
+deck.each do |card|
+  # puts "[" + card[0].to_s + ", \"" + card[1].to_s + "\"]"
+  puts card[0].to_s + " of " + card[1].to_s
+end
