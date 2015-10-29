4099dd8ce2cfe358203c607a82e37143ec6aacb2
Timothy Gregg <timtgregg@gmail.com>
2015-10-29 19:00:36 -0400

    returning values!

--- high_card.rb
+++ high_card.rb
@@ -2,9 +2,11 @@
 
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
-
+players = []
 deck=[]
-
+player_cards=[]
+arena=[]
+winnings=[]
 ranks.each do |rank|
   suits.each do |suit|
     temp_array=[]
@@ -14,6 +16,30 @@ ranks.each do |rank|
   end
 end
 
-puts deck.inspect
 
-players = []
+def value(card, index)
+  case card[index][0]
+  when "A"
+    14
+  when "K"
+    13
+  when "Q"
+    12
+  when "J"
+    11
+  else
+    card[index][0]
+  end
+end
+
+puts deck.shuffle.inspect
+
+puts "#{players.length} players so far. Enter a player name, or 'play':"
+
+user_input=gets.chomp
+
+if user_input != "play"
+  players.push(user_input)
+elsif user_input=="play"
+  puts value(deck.shuffle,0)
+end
