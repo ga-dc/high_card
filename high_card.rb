56425605ae2d3c5c1d972149ddec370f168cdb6c
Elise Kain <elise.kain@gmail.com>
2015-10-29 19:24:48 -0400

    Create deck, shuffle, add players and deal card

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,31 @@
 # Use these two arrays to generate a deck of cards.
+# Card Setup
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
+deck = []
 
+ranks.each do |num|
+  suits.each do |suit|
+    deck.push([num, suit])
+  end
+end
+
+deck.shuffle!
+
+# Game Play
 players = []
+puts players.length.to_s + " players so far. Enter a player name, or type 'play':"
+userInput = gets.chomp
+
+while userInput != "play" do
+  players.push([userInput])
+  userInput = gets.chomp
+end
+
+puts players.inspect
+
+players.each_with_index do |player, index|
+  players[index].push(deck[index])
+end
+
+puts players.inspect
