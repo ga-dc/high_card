5b1461911782537d46279559503ce4ee55444bc1
Davis Green <davismgreen@gmail.com>
2015-10-30 08:51:38 -0400

    adds handling for entering a single player

--- high_card.rb
+++ high_card.rb
@@ -13,7 +13,6 @@ end
 
 puts "#{players.count} players so far. Enter a player name, or type 'play' :"
 new_player = gets.chomp
-
 while new_player != "play"
   players.push(new_player)
   puts "#{new_player} has been added to the game."
@@ -34,10 +33,13 @@ while turns<1 do
   hand.sort!{|a,b| b[1][0] <=> a[1][0]}
   puts hand.inspect
 
-  if hand[0][1][0]== hand[1][1][0]
+if hand.length <2
+  puts "If you play by yourself you'll win every time!"
+  turns +=1
+  elsif hand[0][1][0]== hand[1][1][0]
     puts "There was a tie of the highest card! A " + hand[0][1][1].to_s + ". We're playing another hand!"
   else
     puts hand[0][0] + " won with a " + hand[0][1][1].to_s + " of " + hand[0][1][2]
     turns += 1
-  end
+end
 end
