26e4e8e1ee631cd6e09b7d373d43f931afc6b7bd
MiaKoda Grodsky <mcgrodsky@gmail.com>
2015-10-29 22:21:40 -0400

    comitting after generating a deck of cards using 2 arrays

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,15 @@
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
-
 players = []
+
+# build a deck with ranks, suits, and values
+deck = []
+suits.each do |suit|
+  ranks.each_with_index do |rank, index|
+    deck.push({rank: rank, suit: suit, value: index+2})
+  end
+end
+puts deck
+
+# shuffle deck
