cfb24669c9dec572c2dcda329613e03d95507f20
Britton Walker <bwalker1801@gmail.com>
2015-10-30 08:54:40 -0400

    Assign deck and shuffle

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,15 @@
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
+deck = []
+
+  ranks.each_with_index do |r, i|
+    suits.each do |suit|
+      deck.push({ ranks: r, suits: suit, })
+    end
+  end
+end
+
+  deck.shuffle
 
 players = []
