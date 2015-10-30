827e5470f4b71ba285c734bfa2fe2d2002fdcfca
Kory Campbell <korycampbell@Dwights-MBP.home>
2015-10-29 23:21:23 -0400

    1st commit

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,12 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
-players = []
+
+ranks = %i["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+suits = %i[ "hearts", "spades", "clubs", "diamonds" ]
+
+
+deck = ranks.product(suits).shuffle
+
+puts deck
+
+puts {n} players so far. Enter a player name, or type 'play'
