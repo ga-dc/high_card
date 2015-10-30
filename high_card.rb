b34fff4f55c26aa510a512d3dd55b21d56bc77e1
Wonsik Jeong <wsjeong21@hotmail.com>
2015-10-30 10:08:24 -0400

    still working on it

--- high_card.rb
+++ high_card.rb
@@ -3,3 +3,6 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+# sorry didn't have enough time to do this excercise. I will be doing this over the weekend.
+
+cards ;
