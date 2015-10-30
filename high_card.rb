f2a2e0d23eb0c9e970b7e536eb33968bf822cbf5
Thomas Clark <tclark@soteriagiving.com>
2015-10-29 20:10:43 -0400

    adds array combiner

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,6 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
-
+ranks = %w{"A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" }
+suits = %w{"hearts", "spades", "clubs", "diamonds" }
+suits.each do |suit|
+  ranks.size.times do |i|
 players = []
