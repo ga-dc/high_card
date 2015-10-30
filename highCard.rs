d194e5287144a9da9056c8b86d64213862b23ad1
Benjamin Logan <Blogan@gmail.com>
2015-10-30 10:30:17 -0400

    Initial Commit

---
+++ highCard.rs
@@ -0,0 +1,25 @@
+
+puts "Player 1, what is your name? #{input = playerOneName}"
+puts "Player 2, what is your name? #{input = playerTwoName}"
+
+suit = [clubs, diamonds, hearts, spades]
+
+rank = rank.new
+%w(ace king queen jack).each_with_index {|item, index|
+  rank[item] = index
+}
+
+rank  #=> {"ace"=>14, "king"=>13, "queen"=>12, "jack==>11"}
+rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace]
+
+def play ()
+  cardOne = rank[rand]
+  cardTwo = rank[rand]
+  puts "playerOneName" + ", you were dealt the "cardOne" + " of " + suit[rand] + "."
+  puts "playerTwoName" + ", you were dealt the "cardTwo" + " of " + suit[rand] + "."
+  if cardOne > card Two
+    puts "playerOneName" + ", you win!"
+  elsif cardOne = cardTwo
+    puts "It's a tie between " + "playerOneName" + "and " + "playerTwoName"
+  else
+    puts "playerTwoName" + ", you win!"
