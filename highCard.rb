8d591962e47d6da97ddf17d8cab49b0086e515a6
Benjamin Logan <Blogan@gmail.com>
2015-10-30 10:46:47 -0400

    fixed .rb extension

--- highCard.rb
+++ highCard.rb
@@ -15,9 +15,9 @@ rank = [2, 3, 4, 5, 6, 7, 8, 9, 10, jack, queen, king, ace]
 def play ()
   cardOne = rank[rand]
   cardTwo = rank[rand]
-  puts "playerOneName" + ", you were dealt the "cardOne" + " of " + suit[rand] + "."
-  puts "playerTwoName" + ", you were dealt the "cardTwo" + " of " + suit[rand] + "."
-  if cardOne > card Two
+  puts "playerOneName" + ", you were dealt the  + "cardOne + " of " + suit[rand] + "."
+  puts "playerTwoName" + ", you were dealt the '' + "cardTwo + " of " + suit[rand] + "."
+  if cardOne > cardTwo
     puts "playerOneName" + ", you win!"
   elsif cardOne = cardTwo
     puts "It's a tie between " + "playerOneName" + "and " + "playerTwoName"
