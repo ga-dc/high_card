9c26121515b3f12f734a94d8b6b228f6d5bc942d
Qadriyyyah Thomas <qmadinah@gmail.com>
2015-10-30 00:28:26 -0400

    merged array to variable deck and shuffled

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,22 @@
 # Use these two arrays to generate a deck of cards.
+def cards
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
+deck = []
+
+ranks.each_with_index do |r, i|
+    suits.each do |s|
+      deck.push({
+        ranks: r,
+        suit: s,
+      })
+    end
+  end
+
+return deck.shuffle
+end
+
+
+
 
 players = []
