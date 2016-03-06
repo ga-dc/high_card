5088458811b1963f24b1bb4f2d1bd156cf7f5471
Patrick Moran <patrick_moran@mac.com>
2016-03-06 10:46:20 -0500

    initial commit and build deck

--- high_card.rb
+++ high_card.rb
@@ -2,4 +2,14 @@
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
+deck = []
+
 players = []
+
+ranks.each do |rank|
+  suits.each do |suit|
+    deck.push([rank, suit])
+  end
+end
+
+puts deck.shuffle
