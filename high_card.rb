024acd9d36ae06f11d17fdff9e01c8736502e971
Christine Movius <christine.movius@gmail.com>
2016-03-06 12:06:28 -0500

    function creates deck of 52 cards with index value.

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,13 @@
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
-
+deck = []
 players = []
+
+#Creates Deck with 52 cards and index values for comparing
+ranks.each_with_index do |rank, i|
+  suits.each do |suit|
+    deck.push([rank,suit,i])
+  end
+end
+puts deck
