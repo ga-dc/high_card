916f0b998384ec36d664d741cdd12df5efdd6dde
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-03-06 14:31:31 -0500

    creates deck, shuffles, asks for player name

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,21 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+
+deck = []
+
+ranks.each do |rank|
+  suits.each do |suit|
+    deck.push([rank, suit])
+  end
+end
+
+shuffled = deck.shuffle
+
+def get_players
+  puts " players so far. Enter a player name, or type 'play': "
+  players = gets.chomp
+end
+get_players
