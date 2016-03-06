9005c12d8d0cf66fc5d2bd8219e1d0d1f2f57d27
Patrick Moran <patrick_moran@mac.com>
2016-03-06 13:22:23 -0500

    picks winner

--- high_card.rb
+++ high_card.rb
@@ -1,15 +1,32 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 deck = []
-
+player = ""
 players = []
 
-ranks.each do |rank|
+ranks.each_with_index do |rank, i|
   suits.each do |suit|
-    deck.push([rank, suit])
+    deck.push({value: rank, suit: suit, weight: i})
   end
 end
 
-puts deck.shuffle
+deck.shuffle!
+
+until player == "play"
+  puts "#{players.length} players so far. Enter a player name or type 'play':"
+  player = gets.chomp
+  players.push(player)
+end
+players.pop
+
+  game = []
+  players.each do |player|
+    game.push({player: player, card: deck.pop})
+  end
+  winner = game.max_by do |player|
+    player[:card][:weight]
+  end
+
+  puts "The winner is: " + winner[:player]
