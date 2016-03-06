bb7be673e7f1f400e27f3223156ff8aad5ea7b5d
Christine Movius <christine.movius@gmail.com>
2016-03-06 12:24:43 -0500

    get names of players and push them to array. type play to exit loop.

--- high_card.rb
+++ high_card.rb
@@ -2,15 +2,25 @@
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 deck = []
+player_input=""
 players = []
 
 # Creates Deck with 52 cards and index values for comparing
 ranks.each_with_index do |rank, i|
   suits.each do |suit|
-    deck.push([rank,suit,i])
+    deck << [rank,suit,i + 1]
   end
 end
 
 # Shuffles Deck
 deck = deck.shuffle
 puts deck
+
+# Get names of players and push them to array of players
+while player_input != "play" do
+  puts "#{players.length} players so far. Enter a player name, or type 'play'."
+  player_input = gets.chomp.to_s
+  players << player_input
+end
+players.pop() #Removes 'play' as a player
+puts players
