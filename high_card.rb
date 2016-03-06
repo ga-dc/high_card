0dfe3c0d4fbfb9b60b6561edf78e3cf7a6934b5d
Jeffrey Eaton <jeffreyeaton.dc@gmail.com>
2016-03-06 15:08:03 -0500

    deals two cards and determines winner, prints winner's name

--- high_card.rb
+++ high_card.rb
@@ -1,10 +1,9 @@
 # Use these two arrays to generate a deck of cards.
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
-
 players = []
-
 deck = []
+hand = []
 
 ranks.each do |rank|
   suits.each do |suit|
@@ -14,8 +13,19 @@ end
 
 shuffled = deck.shuffle
 
-def get_players
-  puts " players so far. Enter a player name, or type 'play': "
-  players = gets.chomp
+puts players.length.to_s + " players so far. Enter a player name, or type 'play'"
+players << gets.chomp
+
+2.times do |cards|
+  hand.push(shuffled[cards])
 end
-get_players
+
+puts "Ok, " + players.to_s + " gets " + hand[0].to_s + " and the computer gets " + hand[1].to_s
+
+if hand[0][0] > hand[1][0]
+  puts players.to_s + " wins! Nice one."
+elsif hand[0][0] < hand[1][0]
+  puts "Computer wins. Better luck next time (loser)."
+elsif hand[0][0] == hand[1][0]
+  puts players.to_s + ", looks like you and the computer tied. Better restart the program until Jeff learns enough Ruy to deal with this."
+end
