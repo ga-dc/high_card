cf840e7bb002f6dbd96dd35f2929a9a639f2a08e
Melissa Miller <lis.d.miller@gmail.com>
2016-03-06 18:32:15 -0500

    Adds build_deck, shuffle, and get_players functions

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,31 @@
-# Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
+require "pry"
 
-players = []
+@ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+@suits = [ "hearts", "spades", "clubs", "diamonds" ]
+
+@players = []
+@deck = []
+
+def build_deck
+  @ranks.each do |v|
+    @suits.each do |s|
+      @deck << {value: v, suit: s }
+    end
+  end
+end
+
+def shuffle_deck
+  @deck.shuffle!
+end
+
+def get_players
+  input = 'empty'
+  until input==""
+    puts "Enter a player's name. Press enter when all players are ready."
+    input = gets.chomp
+    next if input == ""
+    @players << input
+  end
+end
+
+binding.pry
