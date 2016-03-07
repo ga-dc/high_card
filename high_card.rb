218251d44d71128e54e1f6026d1c3dc20d6ca544
Melissa Miller <lis.d.miller@gmail.com>
2016-03-07 15:43:18 -0500

    cleans up accessors

--- high_card.rb
+++ high_card.rb
@@ -1,6 +1,7 @@
 require "pry"
 
 class HighCard
+  attr_accessor :players, :deck, :hands
 
   def initialize
     @ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
@@ -20,16 +21,16 @@ class HighCard
   end
 
   def new_game
-    @players = []
-    @deck = []
-    @hands = []
+    self.players = []
+    self.deck = []
+    self.hands = []
     play_game
   end
 
   def build_deck
     @ranks.each_with_index do |v, i|
       @suits.each do |s|
-        @deck << {value: v, suit: s, index: i }
+        deck << {value: v, suit: s, index: i }
       end
     end
   end
@@ -41,23 +42,23 @@ class HighCard
   def get_players
     input = ''
     until input=="play"
-      puts "#{@players.length} players so far. Enter a player name or type 'play':"
+      puts "#{players.length} players so far. Enter a player name or type 'play':"
       input = gets.chomp
       next if input == "play"
-      @players << input
+      players << input
     end
   end
 
   def deal
-    @players.each do |player|
-      card = @deck.pop
+    players.each do |player|
+      card = deck.pop
       puts "#{player}: #{card[:value]} of #{card[:suit]}"
-      @hands << {player: player, value: card[:value], suit: card[:suit], index: card[:index]}
+      hands << {player: player, value: card[:value], suit: card[:suit], index: card[:index]}
     end
   end
 
   def find_highest_card
-    winner = @hands.max_by { |e| e[:index] }
+    winner = hands.max_by { |e| e[:index] }
     puts "Winner is #{winner[:player]} with #{winner[:value]} of #{winner[:suit]}!"
   end
 end
