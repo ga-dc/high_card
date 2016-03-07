ca0e0bfe2c523568df00cd9134792495d6fbb860
Clarissa Stark <clarissa.stark@gmail.com>
2016-03-07 18:28:07 -0500

    added build deck and get player functions

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,25 @@
-# Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
+require "pry"
 
-players = []
+# Use these two arrays to generate a deck of cards.
+@ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+@suits = [ "hearts", "spades", "clubs", "diamonds" ]
+@deck = []
+# hands = []
+@players = []
+
+def build_deck
+  @ranks.each do |rank|
+    @suits.each do |suit|
+      @deck << [ranks, suit]
+    end
+  end
+end
+
+def get_players
+  player = prompt "#{@players.length} so far. Enter a player name, or type 'play':"
+  @players << player
+end
+
+binding.pry
+
+puts "whyyyy"
