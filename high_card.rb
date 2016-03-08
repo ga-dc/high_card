d8527ec699aaede096fd4d71dfcb9c9933cfd02e
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-07 22:39:54 -0500

    deck build method added and some pseudocode

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,22 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
+require "pry"
+
+def build_a_deck
+
+  ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+  suits = [ "hearts", "spades", "clubs", "diamonds" ]
+  deck = []
+  #take one from each rank and match with one from each suit
+  ranks.each_with_index do |rank, index|
+    suits.each do |suit|
+      deck << { number: index, ranks: rank, suits: suit}
+    end
+  end
+end
+
 
 players = []
+
+
+
+binding.pry
