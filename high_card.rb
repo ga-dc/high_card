05f512e937f2353f66a7711324509c52b0248130
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-03-08 14:08:50 -0500

    finished reqs

--- high_card.rb
+++ high_card.rb
@@ -2,15 +2,43 @@ require 'pry'
 
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
-
 players = []
-deck = []
 
-ranks.each_with_index do |rank, index|
-  suits.each do |suit|
-    deck << [rank, suit, index]
+def build_deck(ranks, suits)
+  deck = []
+  ranks.each_with_index do |rank, index|
+    suits.each do |suit|
+      card = {rank: rank, suit: suit, worth: index
+      }
+      deck << card
+    end
   end
+  deck.shuffle
 end
-deck.shuffle!
+
+deck = build_deck(ranks, suits)
+
+while true do
+  puts 'Welcome. Enter your name or press "p" to play.'
+  name = gets.chomp.downcase
+  break if name == 'p'
+  players << name
+end
+
+hand = players.map do |player|
+  deck.pop
+end
+
+currRank = hand.map do |card|
+  card[:worth]
+end
+
+highest = currRank.max
+
+winners = []
+
+currRank.each_with_index do |worth, index|
+  winners.push(players[index]) if worth == highest
+end
 
 binding.pry
