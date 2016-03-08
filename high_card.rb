be6995704d9f7829d8bfa375eb0bfb037b1ed370
Anissa Jones <anissajones10@gmail.com>
2016-03-08 08:51:34 -0500

    deck is now being made and shuffled.

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,24 @@
-# Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
+require 'pry'
 
-players = []
+# Use these two arrays to generate a deck of cards.
+@ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+@suits = [ "hearts", "spades", "clubs", "diamonds" ]
+
+@players = []
+
+def make_deck
+    #for each rank with index add a suit
+    deck = []
+
+    @ranks.each_with_index do |rank, index|
+        @suits.each do |suit|
+            deck << {:rank => rank, :suit => suit, :index => index}
+        end
+    end
+
+    return deck.shuffle!
+end
+
+finaldeck = make_deck
+
+binding.pry
