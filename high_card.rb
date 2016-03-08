3c8c072067aacd518e15a67b8a7825ef9abdf124
Maureen VOGEL <maureenvogel@MaureenOGELsMBP.home>
2016-03-07 22:16:33 -0500

    building deck

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,22 @@
-# Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
+require "pry"
 
-players = []
+def build_deck
+@values = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+@suits = [ "hearts", "spades", "clubs", "diamonds" ]
+@players = []
+@deck = []
+
+@values.each_with_index do |v, i| #each_with_index, adds index value to each item in array
+  @suits.each do |s|
+    @deck.push({score: i, value: v, suit: s})
+  end
+end
+return @deck.shuffle
+end
+
+deck = build_deck #call the build_deck method
+
+def get_players 
+
+
+binding.pry
