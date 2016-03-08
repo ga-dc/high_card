adb033b2254ab046d812121482f6a501a80d3a2c
ALEXA KLEIN <lexi.klein1@gmail.com>
2016-03-08 09:44:06 -0500

    more changes from hw review getting players

--- high_card.rb
+++ high_card.rb
@@ -1,34 +1,11 @@
-# # Use these two arrays to generate a deck of cards.
-# require "pry"
-#
-# def build_deck
-#
-#   ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
-#   suits = [ "hearts", "spades", "clubs", "diamonds" ]
-#   deck = []
-#   #take one from each rank and match with one from each sui
-#       deck << { number: index, ranks: rank, suits: suit}
-#     end
-#   end
-# end
-#
-#
-# players = []
-#
-#
-#
-# binding.pry
-
+require "pry"
+#Use these two arrays to generate a deck of cards.
 #capitalize so they are constant and won't change
 RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 SUITS = [ "hearts", "spades", "clubs", "diamonds" ]
 
-
-#worth gives the index of the rank
-deck = [
-  {rank: "A", suit: "Hearts", worth: 12}
-]
-
+# {rank: "A", suit: "Hearts", worth: 12}
+# deck << card
 
 def build_deck(ranks, suits)
 #build a deck
@@ -38,10 +15,46 @@ def build_deck(ranks, suits)
     #.each with index -> to pull the worth of the card
       #second block variable represents the index of each iteration
     ranks.each_with_index do |rank, index|
-
-    end
+      #don't care about index for suits
+      suits.each do |suit|
     #loop suits
       #push each pair into the deck
+        card = {
+          suit: suit,
+          rank: rank,
+          worth: index
+        }
+        deck << card
+      end
+    end
     #shuffle the deck
-    return deck
+    return deck.shuffle
 end
+
+def choose_players
+  players = []
+  while true do
+    #prompt user to enter name or press play
+    puts "Welcome. Enter your name or press 'p' to play."
+    # get user value
+    #chomp gives you only what was entered
+    name = gets.chomp.downcase # working with strings
+    # take players entered and add to players array
+    players << name
+    # break
+
+  end
+end
+def find_winner
+
+
+end
+
+
+
+#save output of method to a variable
+deck = build_deck(ranks, suits)
+choose_players
+binding.pry
+#fixes pry error
+puts "high card"
