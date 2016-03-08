0dd1879281d4c134cf6e330ff73678ec5293db89
Clarissa Stark <clarissa.stark@gmail.com>
2016-03-08 09:21:03 -0500

    build deck function complete

--- high_card.rb
+++ high_card.rb
@@ -1,20 +1,36 @@
 require "pry"
 
 # Use these two arrays to generate a deck of cards.
-@ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-@suits = [ "hearts", "spades", "clubs", "diamonds" ]
-@deck = []
+RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+SUITS = [ "hearts", "spades", "clubs", "diamonds" ]
+# @deck = []
 # hands = []
 @players = []
 
+card = [
+  {rank: "A", suit: "Hearts", worth: 12}
+]
+deck << card
 
-def build_deck
-  @ranks.each do |rank|
-    @suits.each do |suit|
-      @deck << [ranks, suit]
-      @deck.shuffle
+def build_deck(ranks,suits)
+  # start with empty deck
+  deck = []
+  # loop through ranks
+  RANKS.each_with_index do |rank, index|
+    # loop suits
+    SUITS.each do |suit|
+      # build card hash that's dynamically built after going through the loop
+      card = {
+        suit: suit,
+        rank: rank,
+        worth: index
+      }
+      #push each pair into the deck
+      deck << card
     end
   end
+  deck.shuffle
+  return deck
 end
 
 def get_players
