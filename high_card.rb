f6012efa5b5c227c10114ef994301a2c73b6eeac
Woo Yun <hh747hh@gmail.com>
2016-03-08 14:23:12 -0500

    players set

--- high_card.rb
+++ high_card.rb
@@ -1,4 +1,41 @@
 require 'pry'
+
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ]
+suits = [ "hearts", "spades", "clubs", "diamonds" ]
+players = []
+
+def build_deck(ranks, suits)
+    deck = []
+      ranks.each_with_index do |rank, index|
+        suits.each do |suit|
+          card = {
+            suit: suit,
+            rank: rank,
+            worth: index
+          }
+          deck << card
+        end
+      end
+    return deck.shuffle
+  end
+
+while true
+  puts "#{players.length} so far. Enter a player name, or type 'p'"
+  name = gets.chomp
+
+  break if name == 'p'
+  players << name
+end
+
+
+
+
+shuffledeck = build_deck(ranks,suits)
+binding.pry
+puts "fixes pry error"
+
+=begin
+require 'pry'
 # Use these two arrays to generate a deck of cards.
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
@@ -25,7 +62,6 @@ def build_deck(ranks, suits)
       # shuffle the deck
     return deck.shuffle
 end
-
 def choose_players
   while true do
     puts "Welcome. Enter your name or press 'p' to play"
@@ -36,8 +72,8 @@ def choose_players
 end
 
 
-
 deck = build_deck(ranks, suits)
-choose_players
+#choose_players
 binding.pry
 puts "fixes pry error"
+=end
