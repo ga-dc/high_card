bdab3307d094f941ad816c1f989e1e88cd67f287
winishasmith <winishasmith@gmail.com>
2016-03-08 09:13:31 -0500

    initial commit

--- high_card.rb
+++ high_card.rb
@@ -3,3 +3,19 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+
+deck = [
+  {rank: "A", suit: "hearts", worth: 12}
+]
+
+def build_deck (ranks, suits)
+  # Build a deck
+    # initialize an empty deck
+    deck = []
+    # loop through ranks
+      # .each with index -> to pull the worth of a card
+      # loop suits
+        # push each pair into the deck
+      # shuffle the deck
+    return deck
+end
