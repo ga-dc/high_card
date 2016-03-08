cca43f3d1300a956e35e58c3e550acde94c96d02
Woo Yun <hh747hh@gmail.com>
2016-03-08 13:25:59 -0500

    built the deck

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,43 @@
+require 'pry'
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
+$players = []
 
-players = []
+def build_deck(ranks, suits)
+  # build a deck
+    # initialize an empty deck
+    deck = []
+    # loop through ranks
+      # .each with index -> to pull the worth of a card
+        ranks.each_with_index do |rank, index|
+          suits.each do |suit|
+            # loops suits
+              # push each pair into the deck
+              card = {
+                suit: suit,
+                rank: rank,
+                worth: index
+              }
+              deck << card
+          end
+        end
+      # shuffle the deck
+    return deck.shuffle
+end
+
+def choose_players
+  while true do
+    puts "Welcome. Enter your name or press 'p' to play"
+    name = gets.chomp.downcase # working with strings
+    break if name == 'p'
+    $players << name
+  end
+end
+
+
+
+deck = build_deck(ranks, suits)
+choose_players
+binding.pry
+puts "fixes pry error"
