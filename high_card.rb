d060f51764b0bb259357cb8bb68f5ff5e45f741b
Charles <charlesthwang@gmail.com>
2016-03-08 17:37:37 -0500

    build, shuffle, get players, deals deck

--- high_card.rb
+++ high_card.rb
@@ -1,4 +1,5 @@
 require "pry"
+
 # Use these two arrays to generate a deck of cards.
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
@@ -7,26 +8,44 @@ deck = []
 players = []
 hands = []
 
-# building the deck with each card as a mini-array
+# building the deck with each card as a hash
 ranks.each_with_index do |rank, index|
   suits.each do |suit|
-    deck << {:rank => rank, :suit => suit, :index => index}
+    card = {
+      rank: rank,
+      index: index,
+      suit: suit,
+    }
+    deck << card
   end
 end
 
+#shuffling the deck
 deck.shuffle!
 
 # getting player names and pushing into players array
 print "#{players.length} players so far. Enter a player name, or type 'play':"
-player_name = gets.chomp.capitalize
-players << player_name until player_name == "play"
-
-# pop players.length cards to hands array
-if player_name == "play"
-  card = deck.pop()
-  players.length.times do
-  hands << card
+player_name = nil
+until player_name == "play"
+  player_name = gets.chomp
+  if player_name != "play"
+    players << player_name
+  end
 end
+
+# dealing out a random card to each player
+players.each do |player|
+  # pulls a random card from deck array
+  player_card = deck.sample
+  # pushes pulled card into hands array
+  hands << player_card
+  # removes the card from deck array so the same card can't be dealt again
+  deck = deck - [player_card]
+  # returns dealt card
+  player_card
+end
+
+
 
 
 
