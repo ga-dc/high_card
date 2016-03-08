719059ab83721422d44c01a05211d84d04f576b3
Charles <charlesthwang@gmail.com>
2016-03-08 08:58:01 -0500

    remade deck with each_with_index

--- high_card.rb
+++ high_card.rb
@@ -3,36 +3,30 @@ require "pry"
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
+deck = []
 players = []
+hands = []
 
-#building the deck by getting every combination of ranks and suits arrays
-ranks.each do |value|
-  suits.each {
-    |suit| players << "#{value.to_s} of #{suit}"
-  }
+# building the deck with each card as a mini-array
+ranks.each_with_index do |rank, index|
+  suits.each do |suit|
+    deck << {:rank => rank, :suit => suit, :index => index}
+  end
 end
 
+deck.shuffle!
 
+# getting player names and pushing into players array
+print "#{players.length} players so far. Enter a player name, or type 'play':"
+player_name = gets.chomp.capitalize
+players << player_name until player_name == "play"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# pop players.length cards to hands array
+if player_name == "play"
+  card = deck.pop()
+  players.length.times do
+  hands << card
+end
 
 
 
