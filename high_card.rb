8a6cb6157edf9f761320b6969ddd0448e87e3626
Anissa Jones <anissajones10@gmail.com>
2016-03-08 09:38:13 -0500

    working on get_players in class.

--- high_card.rb
+++ high_card.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 # Use these two arrays to generate a deck of cards.
-@ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+@ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 @suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 @players = []
@@ -9,16 +9,38 @@ require 'pry'
 def make_deck
     #for each rank with index add a suit
     deck = []
-
     @ranks.each_with_index do |rank, index|
         @suits.each do |suit|
             deck << {:rank => rank, :suit => suit, :index => index}
         end
     end
-
+    #return a shuffled deck
     return deck.shuffle!
 end
 
-finaldeck = make_deck
+#save shuffled deck to a variable
+@finaldeck = make_deck
+
+
+
+#"{n} players so far. Enter a player name, or type 'play':"
+
+def get_players
+    # num = 0
+    # p "#{num} players so far. Enter a player name, or type 'play':"
+    # answer = gets.chomp
+    # if answer = "play" do
+    #
+    # else
+    #     @players << answer
+    #     num += 1
+    # end
+    while true do
+        p "#{num} players so far. Enter a player name, or type 'play':"
+        name = gets.chomp
+        #break
+    end
+end
 
 binding.pry
+puts "fixes pry error"
