95147955632a4b7b65952df25412e05565b19c5e
Sanjaya Satyal <sanjaya.k.satyal@gmail.com>
2016-03-07 22:07:22 -0500

    initial commit

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,16 @@
-# Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+require 'pry'
+
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+deck = []
+
+ranks.each do |rank|
+  suits.each do |suit|
+    deck << [rank, suit]
+  end
+end
+deck.shuffle!
+
+binding.pry
