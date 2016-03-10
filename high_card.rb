392ab90e85b071cb6a80a65e5b20af877a07d9e5
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-10 00:10:37 -0500

    fixed typo

--- high_card.rb
+++ high_card.rb
@@ -8,27 +8,27 @@ $players = []
 
 
 def build_deck (ranks, suits)
-    deck = []
-      ranks.each_with_index do |rank, index |
-        suits.each do |suit|
-          card = {
-            suit: suit,
-            rank: ranks,
-            worth: index
-          }
-          deck << card
-        end
-      end
-          return deck.shuffle
+  deck = []
+  ranks.each_with_index do |rank, index |
+    suits.each do |suit|
+      card = {
+        suit: suit,
+        rank: ranks,
+        worth: index
+      }
+      deck << card
+    end
+  end
+  return deck.shuffle
 end
 # how can I make sure this is working? I can't see the single card.
 
 def choose_players
   while true do
     puts "Enter your name or press 'p' to play"
-      name = gets.chomp.downcase #working with strings here
-      break if name === 'p'
-      $players << name
+    name = gets.chomp.downcase #working with strings here
+    break if name === 'p'
+    $players << name
   end
 end
 
