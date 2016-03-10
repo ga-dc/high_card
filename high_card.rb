7da31d3d4e31ab5efa48e1a08c57f3cfaab7914b
Fatai Agiri <yinka.fatai@gmail.com>
2016-03-10 00:02:08 -0500

    get player name and display

--- high_card.rb
+++ high_card.rb
@@ -1,27 +1,18 @@
+require "pry"
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
-players = []
-
-deck = ranks.product(suits)
-
-deck.shuffle
+@players = []
+@hands = []
+deck = []
 
 puts "Enter player name:"
-  user_inputs = gets.chomp.to_s
+user_inputs = gets.chomp.to_s
 
-  if user_inputs = "play"
-    
+  @players.push(user_inputs)
 
-players.push(user_inputs)
-
-  def getPlayer
-    @players << user_inputs
- end
-puts "player name: #{players}"
+puts "player name: #{@players}"
 
 
-
-# user_inputs = gets.chomp.to_s
-# puts "players.push#{user_inputs}"
+binding.pry
