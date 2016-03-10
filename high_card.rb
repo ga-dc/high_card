14f0390516152c90df554c90a857b624b5f9fba4
Brendan Magee <bmagee@gmail.com>
2016-03-09 23:42:00 -0500

    seems to work on a very, very basic level

--- high_card.rb
+++ high_card.rb
@@ -3,11 +3,11 @@ require "pry"
 RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"] #changed the order to reflect aces high
 SUITS = [ "hearts", "spades", "clubs", "diamonds" ]
 PLAYERS = []
+DECK = []
+HANDS = []
 
 def build_deck(ranks, suits)
   # Combine two arrays into a shuffled deck of cards
-  # Create an empty deck array
-  deck = []
     RANKS.each_with_index do |rank, value|
       SUITS.each do |suit|
         #create a card as a hash, using rank, index, and suit
@@ -17,14 +17,14 @@ def build_deck(ranks, suits)
           value: value
         }
         # push the card to the deck array
-        deck << card
+        DECK << card
       end
     end
     #shuffle the resulting deck
-  deck.shuffle
+  DECK.shuffle
 end
 
-def choose_players
+def get_players
   # keep asking for players until the game is done
   while true
     puts "#{PLAYERS.length} players so far. Enter a player name or type 'play' to begin."
@@ -36,14 +36,35 @@ def choose_players
   end
 end
 
-# assign a card to each player, which will be that player's hand
-hands = PLAYERS.map do |player|
-  deck.pop
+# deal a card to each player, which will be that player's hand
+def deal_cards
+  hands = []
+  PLAYERS.each do |player|
+    player_card = {
+      player: player,
+      card: DECK.pop
+    }
+    HANDS << player_card
+  end
 end
 
+# check the hands array for the highest card value
+def get_winner
+  winning_hand = HANDS.max_by do |hand| #had to get help to figure this out
+    hand[:card][:value]
+  end
+  return winning_hand
+end
 
+def play_game
+  build_deck(RANKS, SUITS)
+  get_players
+  deal_cards
+  winner = get_winner
+  puts "The winner is #{winner[:player][:name]}"
+end
 
-
+play_game
 
 binding.pry
 
