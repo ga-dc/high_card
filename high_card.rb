94c0f3b7612e7503dbc5bec28dbd92f8212f8cf5
Woo Yun <hh747hh@gmail.com>
2016-03-09 22:42:18 -0500

    players array

--- high_card.rb
+++ high_card.rb
@@ -5,6 +5,9 @@ suits = [ "hearts", "spades", "clubs", "diamonds" ]
 players = []
 
 def build_deck(ranks, suits)
+
+
+
     deck = []
       ranks.each_with_index do |rank, index|
         suits.each do |suit|
@@ -20,60 +23,35 @@ def build_deck(ranks, suits)
   end
 
 while true
-  puts "#{players.length} so far. Enter a player name, or type 'p'"
+  puts "#{players.length} so far. Enter a player name, or type 'play'"
   name = gets.chomp
 
-  break if name == 'p'
+  break if name == 'play'
   players << name
 end
 
 
+=begin
+#dealing cards to each players
+players.each do |player|
+  #deals random card from deck array
+  player_card = deck.sample
+  player_card[:player] = player
+  #insert random cards into array
+  hands.push(player_card)
+  #removes the card from deck array
+  deck = deck - [player_card]
+end
 
+#find winner
+win_card = hands.max do |hand|
+    hand[:index]
+end
+
+#declare the winner
+puts hands
+puts "#{winning_card[:player]} won with the #{{{winning_card[:rank]} of #{winning_card[:suit]}}}"
 
 shuffledeck = build_deck(ranks,suits)
 binding.pry
 puts "fixes pry error"
-
-=begin
-require 'pry'
-# Use these two arrays to generate a deck of cards.
-ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
-$players = []
-
-def build_deck(ranks, suits)
-  # build a deck
-    # initialize an empty deck
-    deck = []
-    # loop through ranks
-      # .each with index -> to pull the worth of a card
-        ranks.each_with_index do |rank, index|
-          suits.each do |suit|
-            # loops suits
-              # push each pair into the deck
-              card = {
-                suit: suit,
-                rank: rank,
-                worth: index
-              }
-              deck << card
-          end
-        end
-      # shuffle the deck
-    return deck.shuffle
-end
-def choose_players
-  while true do
-    puts "Welcome. Enter your name or press 'p' to play"
-    name = gets.chomp.downcase # working with strings
-    break if name == 'p'
-    $players << name
-  end
-end
-
-
-deck = build_deck(ranks, suits)
-#choose_players
-binding.pry
-puts "fixes pry error"
-=end
