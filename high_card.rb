1e327a46bc9db0c76c85a7175023f37669ef156c
Fatai Agiri <yinka.fatai@gmail.com>
2016-03-10 01:15:43 -0500

    input to get player name and play working

--- high_card.rb
+++ high_card.rb
@@ -4,15 +4,19 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 @players = []
-@hands = []
-deck = []
+@user_inputs = []
+@deck = []
 
-puts "Enter player name:"
-user_inputs = gets.chomp.to_s
 
-  @players.push(user_inputs)
 
-puts "player name: #{@players}"
+until @user_inputs == "play"
+  puts "Please enter player name or type 'p' to play:"
+  @user_inputs = gets.chomp.to_s
+  @players.push(@user_inputs)
+  puts"#{@players}"
+end
+@players.pop
+puts"#{@players}"
 
 
 binding.pry
