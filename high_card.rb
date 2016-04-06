63a59fb7c7edea598f4f951d20a190c55ff333f6
Matt <mvaden08@gmail.com>
2016-04-06 18:30:23 -0400

    created array for names

--- high_card.rb
+++ high_card.rb
@@ -2,9 +2,16 @@
 
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
-players = []
+# players = [
+#    {name: "Jack"}
+#    {name: "George"}
+#    {name: "Benedict"}
+#    {name: "London"}
+# ]
 
 deck = []
 deck = ranks.product(suits)
 
 puts deck.shuffle
+
+players.length 
