f50b7e77b4f00c8c5d48147d23527cde04857232
Hewan Abreha <henebys@gmail.com>
2016-04-06 18:20:43 -0400

    solved shuffling the deck using product

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,31 @@
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
-
 players = []
+
+deck = []
+
+deck = ranks.product(suits)
+ puts deck.shuffle
+
+
+
+
+
+
+
+
+
+
+
+
+# puts each_with_index = each_with_index.shuffle
+# # #This method returns an array of the shuffled elements.
+# # 2
+# # stack_of_cards = stack_of_cards.shuffle
+# # 3
+# #
+# # 4
+# # #This method shuffle it in place - what I would do. :)
+# # 5
+# # stack_of_cards.shuffle!
