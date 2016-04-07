1c50f17913d32c1d5dcd0fc7c468a76d10dfb111
Zaneh Williams <zanehmw@gmail.com>
2016-04-07 07:07:13 -0400

    what I have so far

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,42 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
+# ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+# suits = [ "hearts", "spades", "clubs", "diamonds" ]
+#
+# deck = []
 
+def build_deck
+  values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
+  suits = ['hearts', 'diamonds', 'clubs', 'spades']
+  deck = []
+
+  values.each_with_index do |v, i|
+    suits.each do |s|
+      deck.push({
+        score: i,
+        value: v,
+        suit: s,
+      })
+    end
+  end
+
+  return deck.shuffle
+end
+
+deck = build_deck
 players = []
+num_of_player = 0
+n = players.length
+
+def start
+
+  puts **"{n} players so far. Enter a player name, or type 'play':"**
+  user_input = gets.chomp
+  if user_input == 'play'
+    num_of_player = num_of_player + 1
+    players << 'player(num_of_player)'
+  else players << user_input
+  end
+
+end
+puts start()
+# puts players
