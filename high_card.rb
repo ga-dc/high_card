f5353211eb3d5b3f6dc7bfb8d02ca6f6f2b6b4fb
Zaneh Williams <zanehmw@gmail.com>
2016-04-07 08:44:25 -0400

    i give up

--- high_card.rb
+++ high_card.rb
@@ -1,9 +1,3 @@
-# Use these two arrays to generate a deck of cards.
-# ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-# suits = [ "hearts", "spades", "clubs", "diamonds" ]
-#
-# deck = []
-
 def build_deck
   values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
   suits = ['hearts', 'diamonds', 'clubs', 'spades']
@@ -24,19 +18,17 @@ end
 
 deck = build_deck
 players = []
-num_of_player = 0
-n = players.length
+# num_of_player = 0
+# n = players.length
 
 def start
 
-  puts **"{n} players so far. Enter a player name, or type 'play':"**
+  puts "#{players.length} players so far. Enter a player name, or type 'play':"
   user_input = gets.chomp
-  if user_input == 'play'
-    num_of_player = num_of_player + 1
-    players << 'player(num_of_player)'
-  else players << user_input
-  end
+    break if user_input == 'play'
+    # num_of_player = num_of_player + 1
+    # players << 'player(num_of_player)'
+  players << user_input
+  # end
 
 end
-puts start()
-# puts players
