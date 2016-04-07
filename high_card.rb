5961843db0b8083cd058f4a06489ffe8253c363a
Spicy Legato <jpqreichard@gmail.com>
2016-04-06 21:28:48 -0400

    gets number of players and their names. for daddy. also fixes duplicate Ace rank. for daddy

--- high_card.rb
+++ high_card.rb
@@ -1,9 +1,10 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ]
+ranks = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 deck = []
 face_up_on_the_table = []
 players = []
+number_of_players = nil
 
 ranks.length.times do |rank_inc|
   suits.length.times do |suit_inc|
@@ -12,4 +13,14 @@ ranks.length.times do |rank_inc|
 end
 
 deck.shuffle!
-puts deck
+
+while !(number_of_players.class==Fixnum && number_of_players > 0)
+  puts "How many players?"
+  number_of_players = gets.chomp.to_i
+end
+
+number_of_players.times do |player_num|
+  puts "What is player " + (player_num+1).to_s + "'s name?"
+  player_name = gets.chomp
+  players.push(player_name)
+end
