42bad2a571c759f155b4ef8f35cfa16ba18fe3ea
Wes Reed <wesley.a.reed@gmail.com>
2016-04-07 10:05:27 -0400

    Add code from class review

--- high_card.rb
+++ high_card.rb
@@ -1,12 +1,46 @@
 # Use these two arrays to generate a deck of cards
-  ranks = %w(2 3 4 5 6 7 8 9 10 J Q K A)
-  suits = %w(hearts spades clubs diamonds)
-  deck = []
+  ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+  suits = ["hearts", "spades", "diamonds", "clubs"]
 
-ranks.each do |rank|
-  suits.each do |suit|
-    deck.push [rank, suit]
-  end
+  deck = ranks.product(suits).shuffle
+
+#   deck = []
+#
+#   ranks.each do |rank|
+#     suits.each do |suit|
+#     deck << [rank, suit]
+#   end
+# end
+
+
+players = []
+puts players.length.to_s + "Player so far"
+puts "Enter a player name or type play: "
+player = gets.chomp
+
+while player != "play" do
+  players << player
+  puts "Enter a player name or type play: "
+  player = gets.chomp
 end
 
-puts deck.shuffle
+
+players_with_cards = players.map do |player|
+  {name: player, card: nil}
+end
+
+players_with_cards.each do |player|
+  card = deck.pop
+  player[:card] = card
+end
+
+
+puts players_with_cards.inspect
+
+high_card_value_player = players_with_cards.max do |player|
+  player_rank = ranks.index(player[:card][0])
+  puts player_rank
+  player_rank
+end
+
+puts high_card_value_player[:name] + " has won with " + high_card_value_player[:card].to_s
