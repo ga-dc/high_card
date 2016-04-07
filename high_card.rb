e4ee69051a5e2ba06b05fc33c63ed3c249d455ac
Wes Reed <wesley.a.reed@gmail.com>
2016-04-06 23:21:39 -0400

    Initial commit

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,12 @@
-# Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
+# Use these two arrays to generate a deck of cards
+  ranks = %w(2 3 4 5 6 7 8 9 10 J Q K A)
+  suits = %w(hearts spades clubs diamonds)
+  deck = []
 
-players = []
+ranks.each do |rank|
+  suits.each do |suit|
+    deck.push [rank, suit]
+  end
+end
+
+puts deck.shuffle
