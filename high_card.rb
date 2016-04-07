e8c48f60a6fd2cc66562509ad6ff64514d77d084
Joshua <Joshua.Nezianya@gmail.com>
2016-04-07 07:45:39 -0400

    created deck

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,17 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+
+
+ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
-players = []
+deck = []
+deck = ranks.product(suits)
+puts deck
+# print "Enter player name"
+# player = gets.chomp
+# # player = []
+# puts "Welcome #{player}! Let's play high card!"
+
+
+
+# puts deck.shuffle
