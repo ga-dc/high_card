c264903b4d84b43ea00211d8d728d52180975089
Zaneh Williams <zanehmw@gmail.com>
2016-04-07 10:04:42 -0400

    andys code

--- high_card.rb
+++ high_card.rb
@@ -1,34 +1,42 @@
-def build_deck
-  values = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A']
-  suits = ['hearts', 'diamonds', 'clubs', 'spades']
-  deck = []
-
-  values.each_with_index do |v, i|
-    suits.each do |s|
-      deck.push({
-        score: i,
-        value: v,
-        suit: s,
-      })
-    end
-  end
-
-  return deck.shuffle
+suits = ["spades", "clubs", "hearts", "diamonds"]
+ranks = [2,3,4,5,6,7,8,9,10, "J", "Q", "K", "A"]
+deck = ranks.product(suits).shuffle
+# deck = []
+# ranks.each do |rank|
+#   suits.each do |suit|
+#     deck << [rank, suit]
+#   end
+# end
+players = []
+puts players.length.to_s + "Players so Far"
+puts "Enter a player name or type play: "
+player = gets.chomp
+while player != "play" do
+    players << player
+    puts "Enter a player name or type play: "
+    player = gets.chomp
 end
 
-deck = build_deck
-players = []
-# num_of_player = 0
-# n = players.length
+players_with_cards = players.map do |player|
+  {name: player, card: nil}
+end
 
-def start
+players_with_cards.each do |player|
+  card = deck.pop
+  player[:card] = card
+end
+puts players_with_cards.inspect
 
-  puts "#{players.length} players so far. Enter a player name, or type 'play':"
-  user_input = gets.chomp
-    break if user_input == 'play'
-    # num_of_player = num_of_player + 1
-    # players << 'player(num_of_player)'
-  players << user_input
-  # end
+high_card_value_player = players_with_cards.max do |player|
+  player_rank = ranks.index(player[:card][0])
+end
 
+winners = players_with_cards.select do |player|
+  player[:card][0]  == high_card_value_player[:card][0]
+end
+
+puts high_card_value_player[:name] + " has won with " + high_card_value_player[:card].to_s
+
+if winners.length > 1
+  puts "just kidding was a tie"
 end
