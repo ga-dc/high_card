8437b524b81244b0054339ccae8aa5f26a08bd8e
Spicy Legato <jpqreichard@gmail.com>
2016-04-06 23:15:14 -0400

    ughhhh

--- high_card.rb
+++ high_card.rb
@@ -1,26 +1,42 @@
 # Use these two arrays to generate a deck of cards.
 ranks = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
-deck = []
+game_deck = []
 face_up_on_the_table = []
 players = []
-number_of_players = nil
+input = nil
 
+########## Builds Deck ##########
 ranks.length.times do |rank_inc|
   suits.length.times do |suit_inc|
-    deck.push({rank: ranks[rank_inc], suit: suits[suit_inc]})
+    game_deck.push({rank: ranks[rank_inc], suit: suits[suit_inc]})
+  end
+end
+game_deck.shuffle!
+
+########## Gets Players ##########
+
+until input == "play" do
+  puts (players.length.to_s) + " players so far. Enter a player name, or type 'play':"
+  input = gets.chomp
+  if !(input == "play")
+    players.push({name: input, card: nil})
   end
 end
 
-deck.shuffle!
+puts players
 
-while !(number_of_players.class==Fixnum && number_of_players > 0)
-  puts "How many players?"
-  number_of_players = gets.chomp.to_i
+######### Deals a card to each player ##########
+
+players.length.times do |player_num|
+  card_dealt = game_deck.unshift
+  players[player_num][:card] = card_dealt
 end
 
-number_of_players.times do |player_num|
-  puts "What is player " + (player_num+1).to_s + "'s name?"
-  player_name = gets.chomp
-  players.push(player_name)
+######### Checks for the high card #########
+
+players.length.times do |player_num|
+  face_up_on_the_table[player_num] = players[player_num][:card]
 end
+
+puts face_up_on_the_table
