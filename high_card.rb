5c0fa046460e7a6bc06e21c24db470b5bd870f65
Lauren C. Kennedy <laurenckennedy@gmail.com>
2016-04-07 07:45:17 -0400

    edits ranks array to reflect values

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,5 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = ["A", "K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
