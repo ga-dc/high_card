1ad17235375fa66191fbfecc84b0bd8b00ddfe90
Arthur Adams <arthuradams@gmail.com>
2016-06-18 18:12:14 -0400

    added function to build deck

--- high_card.rb
+++ high_card.rb
@@ -2,4 +2,13 @@
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
-players = []
+def build_deck(ranks, suits)
+  new_deck = []
+  ranks.each do |rank|
+    suits.each do |suit|
+      card = { :rank => rank, :suit => suit}
+      new_deck.push(card)
+    end
+  end
+  return new_deck
+end
