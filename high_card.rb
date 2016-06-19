e84537e674c9d738e2fe1e67566512a7851631e4
Arthur Adams <arthuradams@gmail.com>
2016-06-19 02:22:36 -0400

    added dealing cards to eaxh player, and sorting the hands for scoring

--- high_card.rb
+++ high_card.rb
@@ -1,6 +1,5 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
+
 
 def build_deck(ranks, suits)
   new_deck = []
@@ -33,5 +32,31 @@ def get_players()
   return player_list
 end
 
+def deal(player_list, deck)
+  hand = []
+  player_list.each do |player|
+    card = deck.pop
+    hand.push({:player => player, :card => card})
+  end
+  return hand
+end
 
-players = []
+hash = Hash.new
+%w(cat dog wombat).each_with_index { |item, index|
+  hash[item] = index
+}
+hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
+
+def play_game
+  ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+  suits = [ "hearts", "spades", "clubs", "diamonds" ]
+  indexed_ranks = []
+  deck = build_deck(ranks,suits).shuffle!
+  players = get_players()
+  hand = deal(players, deck)
+#  ranks.each_with_index { |item, index| indexed_ranks[item] = index }
+  hand = hand.sort_by{ |t| ranks.index(t[:card][:rank]) }
+  puts hand.inspect
+end
+
+play_game()
