2d08e5593be1711e4eb43f74d38ad2adc343d4c9
Arthur Adams <arthuradams@gmail.com>
2016-06-19 13:49:40 -0400

    added redealing to tied winners and playing until there's a single winner (or the deck is exhausted)

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,7 @@
-# Use these two arrays to generate a deck of cards.
 
+def pretty_list(list)
+  return list[0..-2].join(", ") + " & "  + list[-1]
+end
 
 def build_deck(ranks, suits)
   new_deck = []
@@ -18,10 +20,10 @@ def get_player(msg)
   return gets.chomp!
 end
 
-def get_players(max)
+def get_players()
   player_list = []
   add_more = true
-  until player_list.size >= 2 and player_list.size <= max and not add_more
+  until player_list.size >= 2 and not add_more
     if player_list.size < 2
       new_name = get_player("There is a minimum of two players. Please add another.")
     else
@@ -29,7 +31,6 @@ def get_players(max)
     end
     new_name.downcase == "play" ? add_more = false : player_list.push(new_name)
   end
-  puts "Cannot add more than " + max.to_s + " players." if player_list.size >= max
   return player_list
 end
 
@@ -37,9 +38,13 @@ def deal(player_list, deck)
   hand = []
   player_list.each do |player|
     card = deck.pop
-    hand.push({:player => player, :card => card})
+    if card
+      hand.push({:player => player, :card => card})
+    else
+      abort("Ran out of cards. Cannot continue. Call it a tie and try 52 Pickup next time.")
+    end
   end
-  return hand
+  return [hand, deck]
 end
 
 def get_winners(hand)
@@ -47,24 +52,42 @@ def get_winners(hand)
   winners.push(hand.pop())
   while winners[-1][:card][:rank] == hand[-1][:card][:rank]
     winners.push(hand.pop())
+    break if hand.size == 0
   end
   return winners
 end
 
+def winners_string(winners_list)
+  return winners_list[0..-2].join(", ") + " & " + winners_lists[-1]
+end
+
+def pretty_list(list)
+  return list[0..-2].join(", ") + " & "  + list[-1]
+end
+
 def play_game()
-  ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+  # Use these two arrays to generate a deck of cards.
+  ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King", "Ace" ]
+#  ranks = [1,2,3,4]
   suits = [ "hearts", "spades", "clubs", "diamonds" ]
   indexed_ranks = []
   deck = build_deck(ranks,suits).shuffle!
-  players = get_players(deck.size)
-  hand = deal(players, deck)
-#  ranks.each_with_index { |item, index| indexed_ranks[item] = index }
-  hand = hand.sort_by{ |t| ranks.index(t[:card][:rank]) }
-  winners = get_winners(hand)
-  if (winners.size == 1)
-    puts "The winner is " + winners[0][:player]
-  else
-    puts "The game is tied!"
+  players = get_players()
+  while (players.size != 1)
+    hand, deck = deal(players, deck)
+    hand = hand.sort_by{ |t| ranks.index(t[:card][:rank]) }
+    winners = get_winners(hand)
+    players = []
+    winner_desc = []
+    winners.each do |winner|
+      players.push(winner[:player])
+      winner_desc.push(winner[:player] + " with the " + winner[:card][:rank].to_s + " of " + winner[:card][:suit])
+    end
+    if (winner_desc.size == 1)
+      puts "The winner is " + winner_desc[0]
+    else
+      puts "The game is tied between " + winner_desc[0..-2].join(", ") + " & "
+    end
   end
 end
 
