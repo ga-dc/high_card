c65be918ef7479090b04b9faffb3b1aa501fe7f7
David <dcordz@gmail.com>
2016-06-20 17:53:36 -0400

    finished everything except reset, no functions

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,33 @@
+require "pry"
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
-
+hands = []
 players = []
+
+deck = []
+suits.each do |suit|
+  ranks.each_with_index do |rank, index|
+    deck.push({rank: rank, suit: suit, score: index})
+  end
+end
+
+deck = deck.shuffle
+
+puts "Enter a number of players."
+moreP = gets.chomp
+moreP.to_i.times do |i|
+  players << "Player #{i+1}"
+end
+
+players.length.times do |i|
+  hands.push({player: players[i], card: deck[i]})
+end
+
+#determine the winer
+high_card = hands.max_by{|player| player[:card][:score]}
+
+puts "#{high_card[:player]} wins!"
+
+binding.pry
+puts "End of file."
