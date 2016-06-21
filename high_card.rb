6b5991f19c976e6e50ecebd2d739863b187bf652
Thaisun Green <Tlgreen16@gmail.COM>
2016-06-21 19:06:00 -0400

    buildDeck Function Builtgit add .! ;)

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,27 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
+require "pry"
 
-players = []
+class PlayGame
+  def initialize
+  end
+
+  players = []
+  def self.buildDeck
+    ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+    suits = [ "hearts", "spades", "clubs", "diamonds" ]
+    deck = []
+    suits.each do |suit|
+      ranks.each do |rank|
+        deck << [rank,suit]
+      end
+  end
+  puts deck
+  end
+  def self.hello
+    puts "hello"
+  end
+
+end
+
+
+binding.pry
