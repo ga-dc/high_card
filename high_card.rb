7d2f9dedc3d0d907ea1fc115cdacc91578df62dd
David <dcordz@gmail.com>
2016-06-22 18:51:09 -0400

    type play_game to begin a new game

--- high_card.rb
+++ high_card.rb
@@ -1,25 +1,31 @@
 require "pry"
 # Use these two arrays to generate a deck of cards.
+def play_game
+
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 hands = []
 players = []
-
 deck = []
+
+#merge ranks and suits and assign scores to each card
 suits.each do |suit|
   ranks.each_with_index do |rank, index|
-    deck.push({rank: rank, suit: suit, score: index})
+    deck << {rank: rank, suit: suit, score: index}
   end
 end
 
+#shuffle deck
 deck = deck.shuffle
 
+#determine number of players
 puts "Enter a number of players."
 moreP = gets.chomp
 moreP.to_i.times do |i|
   players << "Player #{i+1}"
 end
 
+#assign cards
 players.length.times do |i|
   hands.push({player: players[i], card: deck[i]})
 end
@@ -28,6 +34,8 @@ end
 high_card = hands.max_by{|player| player[:card][:score]}
 
 puts "#{high_card[:player]} wins!"
-
 binding.pry
+end
+play_game
+
 puts "End of file."
