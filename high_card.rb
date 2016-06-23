1a2e8038a0ef2c5ef5988e0683652ee3edf8856f
Chris Gardner <ctgardner89@gmail.com>
2016-06-23 10:22:40 -0400

    not quite finished yet

--- high_card.rb
+++ high_card.rb
@@ -3,3 +3,16 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+deck = []
+
+ranks.each do |rnk|
+  suits.each do |sut|
+    deck << [rnk,sut]
+  end
+end
+
+puts "#{players.length} So far,  \n  Enter a name or type 'p' to play"
+choice = gets.chomp
+
+puts deck.shuffle[3]
+puts deck[3]
