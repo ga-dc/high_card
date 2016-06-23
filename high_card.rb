103e23c386f1afc5d4f958c686da1aa1d9cf2330
Thaisun Green <Tlgreen16@gmail.COM>
2016-06-22 21:02:44 -0400

    added sudo code

--- high_card.rb
+++ high_card.rb
@@ -1,6 +1,5 @@
 # Use these two arrays to generate a deck of cards.
 require "pry"
-
 class PlayGame
   @players = []
   @ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
@@ -10,26 +9,37 @@ class PlayGame
   def self.buildDeck
     @suits.each do |suit|
       @ranks.each_with_index do |rank,value|
-        @deck << [[rank,suit],value]
+        @deck << [[:rank rank,:suit suit ,:value value]]
       end
     end
   end
 
   def self.shuffleDeck
       @deck.shuffle
-  end
+    end
+
   def self.deck
     @deck
-  end
-  def self.deal
-    @players.each_with_index do |i,k|
-      @player[i] << deck[k]
     end
-end
+
+  def self.deal
+      @players.count do |k|
+          #for each player
+          #assign them a card from the deck
+          # put each player with the card inside a hand arra
+      end
+    end
 
   def self.players
     @players
   end
+  def self.winner
+      # for each hand
+      # compare the values of each card in players hand
+      # which value is least
+      # that player is the winner 
+  end
+
   def self.addPlayers
     while true do
     puts "There are #{players.count} player(s) and #{players.last} just joined, type your name to add join or type 'play' to begin the game"
@@ -40,10 +50,6 @@ end
           @players << name
         end
     end
-    def self.compare
-        
-    end
-
 
 end
 
