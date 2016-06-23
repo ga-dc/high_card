1b1de71d1ed20f20991a6f6c9dc48b444959126e
Kristin Pasive <kristin.pasive@gmail.com>
2016-06-22 21:36:00 -0400

    added comments aka ROADBLOCKS

--- high_card.rb
+++ high_card.rb
@@ -46,23 +46,24 @@ def deal
     return hands
   end
 
+#ROADBLOCK: Would there be a way to deal the cards and pause before determining the winner?
 
 # Find highest score
 def determine_winner
   cards = deal
   hands = cards.sort_by do |hand, i|
-    hand[:card][2]
+    hand[:card][2] #limits the hand with two arrays
   end
   winner = hands.last[:player]
   winnerCard = hands.last[:card]
-  winnerCard = winnerCard.join(" of ") #I can't seem to figure this part out. I want to just return the rank and the suit, and return the index for score. Example, 7 of hearts instead of 7 of hearts of 6. I don't want the 6 there.
+  winnerCard = winnerCard.join(" of ") #ROADBLOCK: I can't seem to figure this part out. I want to just return the rank and the suit, and return the index for score. Example, 7 of hearts instead of 7 of hearts of 6. I don't want the 6 there.
 
   # score = hands.last[:card].split("")
 
   puts "#{winner} drew the card, #{winnerCard}"
 
   # puts "#{winner}'s score is #{score}'"
-  #I want the index to print out for the score here
+  #ROADBLOCK: I want the index to print out for the score here
 
   puts "#{winner} wins!"
 end
