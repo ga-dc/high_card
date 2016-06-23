16419c15da5ca160c5c0e32c3e8b0344fd4f20d8
Joe Calvillo <jcalvillo757@gmail.com>
2016-06-23 07:45:30 -0400

    Joe Calvillo High Card Ruby HW

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,26 @@
-# Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
+def deck_of_cards
+  ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+  suits = [ "hearts", "spades", "clubs", "diamonds" ]
+  deck = []
 
+  ranks.each_with_index do |value, score|
+    suits.each do |suit|
+      deck.push({
+        score: score,
+        value: value,
+        suit: suit,
+      })
+    end
+  end
+  return deck.shuffle
+end
+
+deck = deck_of_cards
 players = []
+
+while true
+  puts "#{players.length} players. Enter a name, or type play"
+  name = gets.chomp
+  break if name == "play"
+  players.push(name)
+end
