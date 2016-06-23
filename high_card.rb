f69d0d6dbfcead553dfc84852a2ec92118f67893
Andrew Young <prince.of.utah@gmail.com>
2016-06-22 20:17:28 -0400

    final commit

--- high_card.rb
+++ high_card.rb
@@ -1,6 +1,6 @@
 # Use these two arrays to generate a deck of cards.
 # Make/Define a deck of playing cards
-def cards
+def high_cards
   ranks = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
   suits = [ "hearts", "spades", "clubs", "diamonds" ]
   deck = []
@@ -22,7 +22,7 @@ def cards
   end
 
   # Make the players
-  deck = cards
+  deck = high_cards
   players = []
   #{}"{n} players so far. Enter a player name, or type 'play':"
   while true
@@ -30,8 +30,26 @@ def cards
     # Use .chomp to get rid of the record seperator at the end of the string
     # Still not quite sure I understand this
     name = gets.chomp
+  end
 
-    #Upon "play", deal each player a card.
-    #Find the highest card score dealt (Aces high).
+  # Need to turn the players into the cards or vice versa in order to have them be compared in values
+  #Upon "play", deal each player a card.
+cards = players.map do |player|
+  deck.pop
+end
+
+#Find the highest card score dealt (Aces high).
+scores = cards.map do |card|
+  card[:score]
+end
+high_score = scores.max
     #Find the winning player name, then print out:
-    #{}"Winner(s): {name1, name2, …}!"
+    winners = []
+
+    scores.each_with_index do |score, index|
+  winners.push(players[index]) if score == high_score
+end
+
+#{}"Winner(s): {name1, name2, …}!"
+puts "Winner(s): #{winners.join(', ')}"
+puts cards
