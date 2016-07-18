161c07c6bf11af2410004db765342a1d022bf88f
Marc Fournier <marc.fournier91@gmail.com>
2016-07-18 18:03:23 -0400

    Made deck. shuffled it.

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,12 @@
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
-
+deck =[]
+ranks.each do |rank|
+  suits.each do |suit|
+    #deck.push((rank.to_s + " " + suit).split(' '))
+    deck << [rank, suit]
+  end
+end
+deck = deck.shuffle
 players = []
