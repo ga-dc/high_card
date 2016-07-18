584cc23b0ee17e5e8251476bca5d4cc345b07e44
Christopher Phillips <cphillips918@gmail.com>
2016-07-18 18:40:15 -0400

    uhhhh so the global name space is on par with a nuclear disaster....buuuuuuut it works

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,38 @@
 # Use these two arrays to generate a deck of cards.
+$players = []
+$cards = []
+
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
-players = []
+$deck = []
+suits.each do |suit|
+    ranks.each do |rank|
+        $deck.push([suit, rank])
+    end
+end
+puts $deck.inspect
+
+def play()
+    play = true
+    $deck = $deck.shuffle
+    $players.each do |player|
+        $cards.push($deck.sample)
+    end
+    puts $cards.inspect
+    $winner = $cards.each_with_index.max[1]
+    puts $players[$winner] + ' is the winner!'
+    $players = []
+    $cards = []
+end
+
+play = false
+while play == false
+    puts $players.length.to_s + ' players have signed up enter your name to play!'
+    input = gets.chomp
+    if input != 'play'
+        $players.push(input)
+    else
+        play()
+    end
+end
