459c00673b2f334b33893ae27e6f87e544807861
Marc Fournier <marc.fournier91@gmail.com>
2016-07-18 19:27:52 -0400

    Finished the rest of it. I would have commit more but I didn't think I was on the right track. Sorry my code is so hard to read.

--- high_card.rb
+++ high_card.rb
@@ -20,5 +20,39 @@ while true
   end
   players << response
 end
-puts ''
-puts players
+# puts ''
+# puts players
+
+card_hands = []
+players.each_with_index do |player, i|
+  card_hands << [player, deck[i]]  # Array will look like this [player1, [rank, suit]]. We care about card_hands[n][0] and card_hands[n][1][0]
+end
+
+card_hands.each do |hand|
+  puts "Hands : #{hand}"
+end
+
+winner = nil
+winner_index = nil
+highest = 0
+best = nil
+suit = nil
+card_hands.each_with_index do |hand, player|
+  if ranks.index(hand[1][0]) == 0
+    winner = hand[0]
+    best = hand[1][0]
+    suit = hand[1][1]
+    break
+  elsif ranks.index(hand[1][0]) > highest
+    highest = ranks.index(hand[1][0])
+    winner_index = player
+  end
+end
+
+if winner == nil
+  winner = card_hands[winner_index][0]
+  best = card_hands[winner_index][1][0]
+  suit = card_hands[winner_index][1][1]
+end
+
+puts "The winner was #{winner} with a #{best} of #{suit}."
