4009cb441f1200565104a9739f147617b6938d57
AUBREY NORTHAM <AENORTHAM@GMAIL.COM>
2016-07-18 17:30:37 -0400

    mini-array of each card represented

--- high_card.rb
+++ high_card.rb
@@ -2,4 +2,12 @@
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
-players = []
+cards = []
+ranks.each do |val|
+  suits.each do |suit|
+    cards = "#{val}, #{suit}"
+    puts cards
+  end
+end
+
+# players = []
