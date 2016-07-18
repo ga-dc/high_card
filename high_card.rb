25e6b6ab0011263fe3b73881281aa08223cca0d0
Taylor Edwards <tedwards1884@gmail.com>
2016-07-18 19:13:00 -0400

    initial commit; deck creation

--- high_card.rb
+++ high_card.rb
@@ -3,3 +3,16 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+
+deck = []
+
+ranks.each do |rank|
+  suits.each do |suit|
+    card = []
+    card.push(rank)
+    card.push(suit)
+    deck.push(card)
+  end
+end
+
+deck.shuffle!
