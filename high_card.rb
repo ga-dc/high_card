4c2bda8640418634d3fa9d358956806c4736a8d3
Daniela Noir <daniela@danielanoir.com>
2016-07-18 17:30:36 -0400

    first part of HW done

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,21 @@
 # Use these two arrays to generate a deck of cards.
+
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
-players = []
+cards = []
+ranks.each do |rank|
+  suits.each do |suit|
+    cards = "#{rank}, #{suit}"
+    puts cards
+  end
+end
+
+
+
+# 2. Collect an array of player names by prompting:
+# 	* **"{n} players so far. Enter a player name, or type 'play':"**
+# 3. Upon *"play"*, deal each player a card.
+# 4. Find the highest card score dealt (Aces high).
+# 5. Find the winning player name, then print out:
+# 	* **"Winner(s): {name1, name2, …}!"**
