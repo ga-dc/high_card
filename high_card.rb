bdcb2d1c97fc67a76b6f97b4f0d094beffe842ae
Rasheeda Alexander <sheedabeeda@gmail.com>
2016-07-18 19:38:34 -0400

    Built a deck of shuffled cards (an array of cards)

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,23 @@
-# Use these two arrays to generate a deck of cards.
+Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+
+big_daddy = []
+
+ranks.each do |rank|
+  suits.each do |suit|
+    # puts "#{rank} #{suit}"
+    deck = []
+    deck.push(rank)
+    deck.push(suit)
+    puts deck.join(", ")
+
+    big_daddy.push(deck)
+  end
+end
+
+puts big_daddy.join(", ")
+
+puts "Big Daddy Shuffle: " + big_daddy.shuffle.to_s
