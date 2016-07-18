62a3bccb7de08a2a7d141a171616f720edbc1e52
Evan SandersonE <evan.j.sanderson@gmail.coM>
2016-07-18 18:05:42 -0400

    half way there-have cards being assigned to players

--- high_card.rb
+++ high_card.rb
@@ -2,4 +2,39 @@
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
-players = []
+
+deck = []
+
+suits.each do |suit|
+  ranks.each do |rank|
+    deck.push (suit.to_s)
+    deck.push (rank.to_s)
+  end
+end
+
+deck_of_cards = deck.each_slice(2).to_a
+shuffled_deck = deck_of_cards.shuffle
+
+number_of_players=0
+player_names = []
+input = " "
+
+while input != "play"
+  puts "#{number_of_players} players so far. Enter a player name, or type 'play':"
+  input = gets.chomp
+  player_names << input
+  number_of_players+=1
+end
+
+
+if player_names[player_names.length-1] = "play"
+  player_names.pop
+    player_names.each do |player|
+      player_cards = {}
+      player_cards[:"#{player}"] = shuffled_deck.sample
+        puts player_cards.inspect
+          player_cards.each do |hand|
+            puts hand
+          end
+    end
+end
