1a8fb0f94d3dc88dfd1d52eb529e4c798e66ef1e
Daniela Noir <daniela@danielanoir.com>
2016-07-18 19:26:35 -0400

    finished and did the bonusgit add .!

--- high_card.rb
+++ high_card.rb
@@ -1,6 +1,6 @@
 # Use these two arrays to generate a deck of cards.
 
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 cards = []
@@ -12,4 +12,31 @@ end
 
 
 cards_shuffle = cards.shuffle
-puts cards_shuffle
+
+players = []
+puts "'#{players.length}' players so far. Enter player one:"
+name1 = gets.chomp
+players.push(name1)
+while players.length < 2
+puts 'Please add name of player 2:'
+name2 = gets.chomp
+players.push(name2)
+end
+
+
+puts 'Please enter play:'
+play = gets.chomp
+if play = 'play'
+puts hand1 = cards_shuffle.pop
+puts hand2 = cards_shuffle.pop
+puts name1 + " gets " + hand1 + " card."
+puts name2 + " gets " + hand2 + " card."
+end
+
+if hand1 < hand2
+  puts name2 + "wins!"
+elsif hand2 < hand1
+  puts name1 + "wins!"
+else
+  puts name1 + name2 + " tied!"
+end
