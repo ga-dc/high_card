da2d4f646bf4f7c4c08df394972f645dd9aeee13
GIOMAR SILVA <gsilva23@gmail.com>
2016-07-19 07:09:30 -0400

    Couldn't make it shuffle

--- high_card.rb
+++ high_card.rb
@@ -2,4 +2,16 @@
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
+ranks.each do |number|
+  suits.each do |suit|
+    deck = ("#{number} #{suit}")
+    puts deck
+  end
+end
+
+
+
+
+
+
 players = []
