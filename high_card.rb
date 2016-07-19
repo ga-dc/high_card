10049b472ef183f9f284ced727b03b71dc69ec92
Alexander P. Holland <alexanderpholland@Alexanders-MacBook-Air.local>
2016-07-19 08:59:23 -0400

    Added players, struggling

--- high_card.rb
+++ high_card.rb
@@ -1,16 +1,33 @@
-# Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 deck = []
+card = [[]]
+
 
 ranks.each do |rank|
   suits.each do |suit|
 
-    deck.push [rank, suit]
+    card = ["#{rank} #{suit}"]
+    deck.push(card)
   end
 end
 
 deck = deck.shuffle
+
 players = []
-puts deck
+
+while players == []
+  puts "0 players so far. Enter a player name, or type \'play\':"
+  player1 = gets.chomp
+  puts "1 players so far. Enter a player name, or type \'play\':"
+  player2 = gets.chomp
+  if players == 2
+    puts player1 + " and " + player2 + ": Welcome! "
+end
+  handA = [deck.pop[card.to_s]]
+  handB = [deck.pop[card.to_s]]
+
+  puts player1 + " gets " + handA
+  puts player2 + " gets " + handB
+end
