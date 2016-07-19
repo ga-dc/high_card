227112131e39990a1fbd7429056e5b5734d04899
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2016-07-19 06:33:55 -0400

    initial

--- high_card.rb
+++ high_card.rb
@@ -2,4 +2,21 @@
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
-players = []
+#gets shuffled deck
+shuffled_deck = []
+ranks.each_with_index do |rank, score|
+  suits each do |suit|
+    shuffled_deck.push(score, rank, value)
+  end
+end
+
+#gets the two players
+highrollers = []
+puts "Enter your name highroller1"
+highroller1 = gets.chomp
+highrollers.push(highroller1)
+while highrollers.length < 2
+  puts "Please enter your name highroller2"
+  highroller2 = gets.chomp
+  highrollers.push(highroller2)
+end
