528ab1536b4303371573e5c9acc3a1d699917fda
Megan Coyle <meg@mcoyle.com>
2016-07-18 20:31:25 -0400

    Reorganize cards for values, test logic for card scoring

--- high_card.rb
+++ high_card.rb
@@ -1,28 +1,43 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 deck = []
 
 players = []
 
 # create deck
-ranks.each do |rank|
+# ranks.each do |rank|
+#   suits.each do |suit|
+#     deck << [rank, suit]
+#   end
+# end
+
+ranks.each_with_index do |rank, index|
   suits.each do |suit|
-    deck << [rank, suit]
+    deck << ({
+      score: index,
+      rank: rank,
+      suit: suit
+    })
   end
 end
 
-# get players
-while true
-  puts "#{players.length} so far. Enter a player name, or type 'play':"
-  name = gets.chomp
-
-  break if name == "play"
-  players.push(name)
+deck.each do |deck|
+  puts deck[:score]
 end
 
-# give each player a card
-for player in players do
-  # give them a card from the shuffled deck
-  users_card = deck.shuffle.pop()
-end
+# # get players
+# while true
+#   puts "#{players.length} so far. Enter a player name, or type 'play':"
+#   name = gets.chomp
+#
+#   break if name == "play"
+#   players.push(name)
+#
+# end
+#
+# # give each player a card
+# for player in players do
+#   # give them a card from the shuffled deck
+#   users_card = deck.shuffle.pop()
+# end
