ede982e2760511e17d6ccd3c9351670ae92c46f3
Christopher Phillips <cphillips918@gmail.com>
2016-07-18 23:11:37 -0400

    bonus fail...lets talk tomorrow about ruby

---
+++ jesus.rb
@@ -0,0 +1,53 @@
+##Step 1 - Make the deck =)
+def deck()
+    ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+    suits = [ "hearts", "spades", "clubs", "diamonds" ]
+    deck = []
+
+    suits.each do |suit|
+        ranks.each_with_index do |rank, index|
+            deck.push([suit, rank, index])
+        end
+    end
+    return deck.shuffle
+end
+
+##Step 2 - Get Player Names
+$my_deck = deck()
+$players = []
+
+while true
+    puts $players.length.to_s + ' players have signed up to play! Enter your name to play the game! Enter play to start the game'
+    player = gets.chomp
+    break if player == 'play'
+    $players.push(player)
+end
+
+##Step 3 BREAK THAT LOOP!
+def play()
+    $my_cards = $players.map {|player| $my_deck.pop}
+
+    ##Step 4 Ahh yea winner time
+    $winner = []
+    $my_cards.each do |card|
+        $winner.push(card[2])
+    end
+
+    new_array = $winner.uniq
+
+    if new_array.max == $winner.max
+        puts 'Its a tie! Redeal'
+        play()
+    else
+        $champ = $winner.index($winner.max)
+    end
+    return $players[$champ] + ' is the winner with a ' + $my_cards[$champ].inspect
+end
+
+puts play()
+
+##Tie Breaker - Call winner.uniq (this returns new array and removes duplicates)
+##IF New Array.max and old array.max are the same then reshuffle because tie
+##I DONT KNOW HOW TO DO THAT YET BUT I GET IT
+##If winner.lenght
+##THIS IS WHERE THE BONUS GOES AND I CAN"T FIGURE IT OUT!
