7d24e31f2388151338957c7d504770f96713dad7
Ahmed Elkheshin <ahmed.elkheshin@gmail.com>
2016-07-18 20:51:29 -0400

    Added player input. Prints deck array for each player.

--- high_card.rb
+++ high_card.rb
@@ -12,19 +12,33 @@ end
 
 ranks.each do |rank|
   suits.each do |suit|
-    puts "#{rank} #{suit}"
+    # puts "#{rank} #{suit}"
     deck.push([rank, suit])
     deck.shuffle!
   end
 end
 
-puts deck.to_s
+players = []
 
-players = [{
-            name = ""
-            hand = []
-            score = 0
-  }]
 
-puts '{n} players so far. Enter a player name, or type play'
-gets.chomp
+
+# puts "#{players.length} players so far. Enter a player name, or type play"
+while true
+  puts "Enter name"
+  name = gets.chomp
+  if name == "play"
+    break
+  end
+  players.push name
+end
+puts players
+
+table = [ ]
+
+players.length.times do
+  table.push deck.pop
+end
+puts table.to_s
+
+#  while or until loops
+# each_with_index
