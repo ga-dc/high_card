4f8494167aa00267d47d47c62bf973c61605ed45
Tim Foley <thetimfoley@gmail.com>
2016-09-26 16:38:09 -0400

    initial commit

--- high_card.rb
+++ high_card.rb
@@ -3,3 +3,13 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+
+deck = suits.map do |suit|
+  set = ranks.map do |rank|
+    [rank, suit]
+  end
+end
+
+deck.flatten!(1)
+
+p deck
