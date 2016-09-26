370726a112d7bafe655baad65b9641bdd03b9620
Michael Rubin <michael@rubinsworld.com>
2016-09-26 19:30:18 -0400

    deck shuffleld

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,15 @@
-# Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
-
+deck = []
 players = []
+# 1. Build a deck of shuffled cards.
+#	* The deck should be an array of cards.
+#	* Each card should be a mini-array with a value and suit.
+#     Example: `[8"hearts"]`
+#	* Shuffle the deck. It would look something like this...
+ranks.each do |rank|
+	suits.each do |suit|
+		deck.push([rank,suit])
+	end
+end
+deck.shuffle!
