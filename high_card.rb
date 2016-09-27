070c6c59bdd74b7283e4bd14a2755bd161bc6c93
Barrett Cope <barrettcope@gmail.com>
2016-09-26 23:14:47 -0400

    Deck built

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,12 @@
+require 'pry'
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
+deck = []
+
+suits.each { |suit| ranks.each { |rank| deck << ["#{suit}","#{rank}"]}}
 
 players = []
+
+binding.pry
+puts 'done'
