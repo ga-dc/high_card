4658132e93534c57501de87b2a077b95c1588c09
Barrett Cope <barrettcope@gmail.com>
2016-09-27 00:15:48 -0400

    Deal cards. Bug with printing dealt cards to screen.

--- high_card.rb
+++ high_card.rb
@@ -1,12 +1,21 @@
 require 'pry'
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
-deck = []
-
+deck, players, winners, play = [], [], [], false
 suits.each { |suit| ranks.each { |rank| deck << ["#{suit}","#{rank}"]}}
-
-players = []
-
+# prompt user for input
+while play == false do
+  puts "#{players.length} players so far. Enter a player name, or type 'play':"
+  input = gets
+  # check input for play
+  input.chomp != 'play' ? players << {name: input.chomp} : play = true
+end
+deck.shuffle!
+players.each { |player| player[:card] = deck.pop }
+# this is not working
+players.each { |player| puts "#{player[:name]} got the #{player[:card[1]]} of #{player[:card[0]]}."}
+# evaluate rank against ranks index
+# iterate over players array to get index value of
 binding.pry
 puts 'done'
