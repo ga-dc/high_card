917f5fee004fc3b57e981f154740a36faeca607d
Andrew Whitley <awhitley1233@gmail.com>
2016-09-27 16:37:27 -0400

    created auto generating/shuffled deck

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,21 @@
+require 'pry'
+
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+
+
+deck = []
+
+
+suits.each do |suit|
+  ranks.each do |rank|
+    deck << [rank, suit]
+  end
+end
+
+deck.shuffle!
+
+binding.pry
