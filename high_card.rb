2a1c4833067d3cddfda70010ec682212d2dcb0cf
YOUR FULL NAME <lun.mariancik@gmail.com>
2016-09-27 13:47:26 -0400

    first part completed

--- high_card.rb
+++ high_card.rb
@@ -2,16 +2,30 @@ require 'pry'
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
-# players = []
 
 
 deck = []
-ranks.each_with_index do |rank, score|
+ranks.each_with_index do |rank|
   suits.each do |suit|
   deck << [rank,suit]
+  # deck << "#{rank}, #{suit}"
   end
 end
 deck = deck.shuffle
+
+
+
+# players = []
+
+
+
+
+
+
+
+
+
+
 
 binding.pry
 puts "stop pry bug"
