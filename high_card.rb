3162c688c241c90f79e33a871b69ac3143ae0a79
Andrew Whitley <awhitley1233@gmail.com>
2016-09-27 18:20:13 -0400

    game can determine winner (including ties)

--- high_card.rb
+++ high_card.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
@@ -20,10 +20,24 @@ loop do
   puts "#{players.length} Players currently. Enter a player name or type 'play' to begin"
   player_name = gets.chomp
   if player_name != 'play'
-    players << player_name
+    players << {name: player_name}
   end
 break if player_name == 'play'
 end
 
+players.each do |player|
+  random_card = deck[rand(deck.length - 1)]
+  player[:card] = random_card
+end
+
+
+
+  card_values = players.map{ |player| ranks.find_index(player[:card][0])}
+
+
+  high_value = card_values.max
+  winner = players.select{ |player| ranks.find_index(player[:card][0]) == high_value}
+  winner_name = winner.map{ |winner| winner[:name]}.join(' and ')
+puts "Winner(s) : #{winner_name}"
 
 binding.pry
