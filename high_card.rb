1a7d848c3e2db295a67b9f8f5a2e517ecac25723
YOUR FULL NAME <lun.mariancik@gmail.com>
2016-09-26 20:48:38 -0400

    first commit <made an array of arrays>

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,17 @@
+require 'pry'
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
+# players = []
 
-players = []
+
+deck = []
+ranks.each_with_index do |rank, score|
+  suits.each do |suit|
+  deck << [rank,suit]
+  end
+end
+deck = deck.shuffle
+
+binding.pry
+puts "stop pry bug"
