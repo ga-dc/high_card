aaa5093699c537727df4f48774cf0d024e3fd43e
Barrett Cope <barrettcope@gmail.com>
2016-09-27 00:38:25 -0400

    Print dealt cards to screen. Bug with getting index value of rank.

--- high_card.rb
+++ high_card.rb
@@ -13,9 +13,10 @@ while play == false do
 end
 deck.shuffle!
 players.each { |player| player[:card] = deck.pop }
-# this is not working
-players.each { |player| puts "#{player[:name]} got the #{player[:card[1]]} of #{player[:card[0]]}."}
+players.each { |player| puts "#{player[:name]} got the #{player[:card][1]} of #{player[:card][0]}."}
+# this is being set to nil - not sure why
+players.each { |player| player[:card_value] = ranks.index(player[:card][1])}
+binding.pry
 # evaluate rank against ranks index
 # iterate over players array to get index value of
-binding.pry
 puts 'done'
