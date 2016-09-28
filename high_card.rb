669042addc63e936bc035fdedbdc4c489ee13663
Michael Rubin <michael@rubinsworld.com>
2016-09-28 14:23:55 -0400

    Final release with bonus 2

--- high_card.rb
+++ high_card.rb
@@ -6,15 +6,15 @@
 #######################################################################
 def rankval ( rank )
   if rank.is_a? Numeric
-  	return rank
-  	elsif rank =="J"
-  	 return (11)
-  	elsif rank =="Q"
-  	 return (12)
-  	elsif rank =="K"
-  	 return (13)
-  	elsif rank =="A"
-  	 return (14)
+    return rank
+  elsif rank =="J"
+    return (11)
+  elsif rank =="Q"
+    return (12)
+  elsif rank =="K"
+    return (13)
+  elsif rank =="A"
+    return (14)
   end	 #if
 end  # rankval
 
@@ -27,31 +27,31 @@ end  # rankval
 #######################################################################def print_card(c)
 def rank_name(rank)
   if rank==2
-  	return "Two"
+    return "Two"
   elsif rank ==3
-   return ("Three")
+    return ("Three")
   elsif rank ==4
-   return ("Four")
+    return ("Four")
   elsif rank ==5
-   return ("Five")
+    return ("Five")
   elsif rank ==6
-   return ("Six")
+    return ("Six")
   elsif rank ==7
-   return ("Seven")
+    return ("Seven")
   elsif rank ==8
-   return ("Eight")
+    return ("Eight")
   elsif rank == 9
-   return ("Nine")
+    return ("Nine")
   elsif rank ==10
     return ("Ten")
   elsif rank =="J"
     return ("Jack")
   elsif rank =="Q"
-   return ("Queen")
+    return ("Queen")
   elsif rank =="K"
-   return ("King")
+    return ("King")
   elsif rank =="A"
-   return ("Ace")
+    return ("Ace")
   end	 #if
 end # rank_name
 
@@ -65,12 +65,50 @@ end # rank_name
 #######################################################################
 def print_card ( person, players)
   players.each do |player|
-   if person == player[:player]
-     print (" #{person} won with a #{rank_name(player[:rank])} of #{player[:suit]} ")
-   end # if
+    if person == player[:player]
+      print (" #{person} won with a #{rank_name(player[:rank])} of #{player[:suit]} ")
+    end # if
   end # each
 end # print_card
 
+
+#######################################################################
+# list_winners
+# Description: Print out winner name list(s)
+#     Parmeters:  winners- array of strings of player names
+#
+#     Return - None
+#######################################################################
+def list_winners (winners_list)
+
+  for w in winners_list
+
+    # handle case of more than one winner but not last winner
+    if (winners_list.length > 1) && ( w != winners_list.last) && (w != winners_list[0] )
+      sep = ", "
+    elsif (winners_list.length > 1) && ( w == winners_list.last)
+      sep =" and "
+    end # if
+    print "#{sep}"
+    print "#{w}"          # print name
+  end #for w
+end # function list_winners
+
+#######################################################################
+# list_cards
+# Description: Print card values for each winner
+#     Parmeters:  highcard - value of winning card
+#                 Game - Array of all players in game
+#     Return - None
+#######################################################################
+
+def list_cards( winners,game)
+  for c in winners
+    print_card(c,game)
+    puts ""  # force new line
+  end # for c
+
+end # list_cards
 
 #######################################################################
 # announce_winners
@@ -82,83 +120,83 @@ end # print_card
 
 def announce_winners(highcard,game)
 
-# now announce each winner that has highcard
- winners=[]
- game.each do |user|
- 	if user[:cardval] == highcard
- 	 winners.push(user[:player])
-     end # if
- end # game each
+  # now announce each winner that has highcard
+  winners=[]
+  game.each do |user|
+    if user[:cardval] == highcard
+      winners.push(user[:player])
+    end # if
+  end # game each do
 
 
-# BONUS if more than one winner Announce this
- numwinners = winners.length > 1 ?" There is a tie between " : " There is one winner "
-
- print "#{numwinners}"
-  idx=0
-	for c in winners
-		print "#{c}, "
-    print_card(c,game)
-    if idx > 0
-      puts "," # we have more than one winner so put a comma
-    end #if
-	end # for c
-  puts ""  # force new line
+  # BONUS if more than one winner Announce this
+  print "#{ (winners.length > 1) ? ' There is a tie between ' : ' There is one winner '}"
+  list_winners(winners)
+  list_cards(winners,game)
 end #announce_winners
 
 
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
-deck = []
-players = []
-# 1. Build a deck of shuffled cards.
-#	* The deck should be an array of cards.
-#	* Each card should be a mini-array with a value and suit.
-#     Example: `[8"hearts"]`
-#	* Shuffle the deck. It would look something like this...
-ranks.each do |rank|
-	suits.each do |suit|
-		deck.push([rank,suit])
-	end
-end
-deck.shuffle!
+  #######################################################################
+  # MAIN LINE BEGINS HERE
+  #######################################################################
 
-# Collect an array of player names by prompting:
-#
-#"{n} players so far. Enter a player name, or type 'play':
-#   " Should look like this: [{name:'Jesse'}, {name:'Jane'}]
-#
+  ######################
+  # Global Variables
+  ######################
 
-puts "Welcome to High Card"
-last_user=""
-until last_user.upcase=="PLAY"
-	puts "#{players.length} players so far. Enter a player name or type 'play'"
-	last_user = gets.chomp!
-	if last_user.upcase != "PLAY"
-		players.push(last_user)
-	end  # if
-end  #until
+  ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+  suits = [ "hearts", "spades", "clubs", "diamonds" ]
+  deck = []
+  players = []
 
-# weight each card based on values for high card determination
+  # 1. Build a deck of shuffled cards.
+  #	* The deck should be an array of cards.
+  #	* Each card should be a mini-array with a value and suit.
+  #     Example: `[8"hearts"]`
+  #	* Shuffle the deck. It would look something like this...
+  ranks.each do |rank|
+    suits.each do |suit|
+      deck.push([rank,suit])
+    end
+  end
+  deck.shuffle!
+
+  # Collect an array of player names by prompting:
+  #
+  #"{n} players so far. Enter a player name, or type 'play':
+  #   " Should look like this: [{name:'Jesse'}, {name:'Jane'}]
+  #
+
+  puts "Welcome to High Card"
+  last_user=""
+  until last_user.upcase=="PLAY"
+    puts "#{players.length} players so far. Enter a player name or type 'play'"
+    last_user = gets.chomp!
+    if last_user.upcase != "PLAY"
+      players.push(last_user)
+    end  # if
+  end  #until
+
+  # weight each card based on values for high card determination
 
 
-#Upon "play", deal each player a card.
+  #Upon "play", deal each player a card.
 
-game=[]
- players.each do |player|
- 	card=deck.pop
+  game=[]
+  players.each do |player|
+    card=deck.pop
     hash={player:player,rank:card[0],suit:card[1],cardval:rankval(card[0])}
     game.push(hash)
- end # each
+  end # each
 
- # now figure out which player has the max card
+  # now figure out which player has the max card
 
- playercardvals=[]
- game.each do |cardval|
- 	playercardvals.push(cardval[:cardval])
- end
+  playercardvals=[]
+  game.each do |cardval|
+    playercardvals.push(cardval[:cardval])
+  end #each
 
-# get highest card value
-highcard=playercardvals.max
+  # get highest card value
+  highcard=playercardvals.max
 
-announce_winners(highcard,game)
+  announce_winners(highcard,game)
