124098253ea43d7845c9e4a2d06e7c6da518e65c
William Keating <keating49@gmail.com>
2016-09-28 13:25:18 -0400

    deck shuffled

--- high_card.rb
+++ high_card.rb
@@ -3,3 +3,13 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+full_deck = []
+
+suits.each do |suit|
+  ranks.each_with_index do |rank|
+    full_deck << [suit, rank]
+    full_deck.shuffle!
+
+  end
+end
+  p full_deck
