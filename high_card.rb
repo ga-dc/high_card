e8fad7a80d21b7c4bcfb5fc04a73ac8c7efaad9b
Alex Manno <alman92@gmail.com>
2016-09-28 18:19:39 -0400

    finsihed question 3

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,36 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+require 'pry'
+
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+deck = []
+
+index = 0
+
+ranks.each do |rank|
+suits.each do |suit|
+  deck.push [rank, suit]
+end
+end
+
+deck.shuffle!
+
+loop do
+  name = gets.chomp
+  if name != 'play'
+    players.push name
+  end
+  break if name == 'play'
+end
+
+players.each do |player|
+  players[:card] = deck.pop
+  #[:card] = property
+  #.pop returns the last item of an array
+end
+
+
+binding.pry
+puts "stop pry bug"
