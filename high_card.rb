67e9c05b4625b6327ca6eb0ace452dc5e16e9c97
Barrett Cope <barrettcope@gmail.com>
2016-09-27 20:42:34 -0400

    Add tie handling.

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,3 @@
-require 'pry'
-deck, players = [], []
 ranks = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
@@ -9,34 +7,30 @@ def setup_deck deck, ranks, suits
 end
 
 def get_players
+  play, players = false, []
   while play == false do
     puts "#{players.length} players so far. Enter a player name, or type 'play':"
-    input = gets
-    # check input for play
-    input.chomp != 'play' ? players << {name: input.chomp} : play = true
+    input = gets.chomp
+    input != 'play' ? players << {name: input} : play = true
   end
   players
 end
 
 def play players, ranks, suits
-  winners = []
   deck = setup_deck [], ranks, suits
   players.each do |player|
     player[:card] = deck.pop
     puts "#{player[:name]} got the #{player[:card][1]} of #{player[:card][0]}."
   end
-  winners << players.max_by { |player| player[:card][2] }
-  binding.pry
+  max = players.max_by { |player| player[:card][2] }
+  winners = players.select { |player| player[:card][2] == max[:card][2] }
   if winners.length == 1
-    puts "The winner is #{winners[0][:name]} with the #{player[:card][1]} of #{player[:card][0]}."
+    puts "The winner is #{winners[0][:name]} with the #{winners[0][:card][1]} of #{winners[0][:card][0]}.\nGame Over"
   else
-    puts "There was a #{winners.length} way tie."
-    players = players.each { |player| player.delete(:card) }
-    play players [], ranks, suits
+    puts "There was a #{winners.length} way tie. Tied players will be dealt a new card."
+    players = winners.each { |winner| winner.delete(:card) }
+    play players, ranks, suits
   end
-  puts "Game Over"
 end
 
 play get_players, ranks, suits
-
-puts 'done'
