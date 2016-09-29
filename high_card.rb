3fa54cac87bf6d6a1022f2b800b7e1c3f4f9d486
brown2206 <brown.angelo.jr@gmail.com>
2016-09-29 04:10:07 -0400

    added functionality

--- high_card.rb
+++ high_card.rb
@@ -3,3 +3,19 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+deck = []
+
+ranks.each do |rank|
+suits.each do |suit|
+new_card = [rank,suit]
+puts new_card.inspect  #Successfully generate playing deck of 52 cards
+deck << new_card
+deck.shuffle!
+puts deck
+
+puts "#{players.length} players so far.  Enter a player name, or type 'play':"
+player_response = gets.chomp
+
+players << player_response
+end
+end
