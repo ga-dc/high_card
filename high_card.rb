eccfe99c4de99b8bf1f12fbb19ea6e9a40a5b931
YOUR FULL NAMEAndres <a_espinosa@live.com>
2016-09-29 00:42:09 -0400

    commit

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,32 @@
-# Use these two arrays to generate a deck of cards.
+require 'pry'
+
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
+
 players = []
+deck = []
+
+suits.each do |suit|
+highest.each do |highest|
+    deck << {highest :highest, suit :suit}
+  end
+end
+
+deck.shuffle!
+
+loop do
+  puts "#{players.length}"number of players.  Enter a player name!"
+  player_name = gets.chomp
+  if player_name != 'play'
+    players << {name: player_name}
+  end
+end
+
+players.each do |player|
+  someCard = deck[rand(deck.length)]
+  player[:card] = someCard
+end
+
+binding.pry
+puts "done but not done..."
