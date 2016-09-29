6b09612059ffe30bc1c1e7a5e5a465dfd2476574
William Keating <keating49@gmail.com>
2016-09-29 08:38:04 -0400

    deck and players

--- high_card.rb
+++ high_card.rb
@@ -1,3 +1,4 @@
+require "pry"
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
@@ -9,7 +10,27 @@ suits.each do |suit|
   ranks.each_with_index do |rank|
     full_deck << [suit, rank]
     full_deck.shuffle!
-
   end
+  class Player
+    def initialize(name)
+      @@players = []
+      @name = name
+      @@players << self
+    end
+    def get_name
+      @name = gets.chomp
+  end
+    def Player.players
+      @@players
+    end
 end
-  p full_deck
+end
+
+billy = Player.new "Billy"
+p billy
+p "whats your name?"
+input = gets.chomp
+Player.new (input)
+p Player.players
+binding.pry
+puts "done"
