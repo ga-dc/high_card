afe1f7cf4edf388f28fd536ecbe05d20759503ec
Doray Gisele Owens <doray.owens@gmail.com>
2016-09-28 21:18:09 -0400

    high card hw

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,11 @@
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
+deck = ranks.product(suits).shuffle
+player_one = " "
+player_two = " "
 
-players = []
+players = [0]
+
+p "{n} players so far. Enter a player name, or type 'play':"
+  [{name: player_one}, {name: player_two}] = gets.chomp
