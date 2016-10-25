925a0067cbee5558504f59423904e0156cecf98c
Rob Romaniw <robertromaniw@gmail.com>
2016-10-25 12:30:36 -0400

    Commit 2: Deck Shuffles

--- high_card.rb
+++ high_card.rb
@@ -1,17 +1,19 @@
-# Use these two arrays to generate a deck of cards.
-# ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-# suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
-# players = []
 
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 deck = []
 
 suits.each do |suit| #loop through the suits
-  ranks.each do |rank| #loop through all ranks per suit
+  ranks.each_with_index do |rank| #loop through all ranks per suit
     deck << [suit, rank] #adds rank/suit object to deck array
   end
 end
-puts deck
+
+deck.shuffle! # shuffle deck
+
+puts ""
+puts deck.inspect
+puts ""
 puts deck.length
+puts ""
