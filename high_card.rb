4f313614613df8479ced6219fa36f8c772661a7f
Angel Valant <angelgithub@gmail.com>
2016-10-24 21:01:58 -0400

    updates high_card.rb

--- high_card.rb
+++ high_card.rb
@@ -2,4 +2,22 @@
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
+deck = []
+
 players = []
+
+#create card decck
+suits.each do |suit|
+  ranks.each do |rank|
+    deck << [suit, rank]
+  end
+end
+
+puts deck
+
+
+#accept player names
+puts "Enter player names:"
+user_input = gets.chomp
+players.push(user_input)
+puts players
