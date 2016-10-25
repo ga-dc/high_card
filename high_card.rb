f7329828dbfc7fa58bae6bb9d2eac3b03d31580a
Rob Romaniw <robertromaniw@gmail.com>
2016-10-25 12:24:18 -0400

    Commit 1: Deck is created

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,17 @@
 # Use these two arrays to generate a deck of cards.
+# ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+# suits = [ "hearts", "spades", "clubs", "diamonds" ]
+
+# players = []
+
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
+deck = []
 
-players = []
+suits.each do |suit| #loop through the suits
+  ranks.each do |rank| #loop through all ranks per suit
+    deck << [suit, rank] #adds rank/suit object to deck array
+  end
+end
+puts deck
+puts deck.length
