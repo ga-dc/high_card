951a5ae9daddb932da6394a5daba97c8d7febf4d
Angel Valant <angelgithub@gmail.com>
2016-10-24 21:57:44 -0400

    updates high_card game, players array is correct

--- high_card.rb
+++ high_card.rb
@@ -1,17 +1,16 @@
 # Use these two arrays to generate a deck of cards.
 # ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 # suits = [ "hearts", "spades", "clubs", "diamonds" ]
-
 # deck = []
-
 # players = []
 
-#create card decck
+# create card deck
 def create_cards ()
   ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
   suits = [ "hearts", "spades", "clubs", "diamonds" ]
   deck = []
 
+  # nested loops create mini-array in decks array
   suits.each do |suit|
     ranks.each do |rank|
       deck << [suit, rank]
@@ -20,13 +19,20 @@ def create_cards ()
   puts deck.shuffle
 end
 
-create_cards()
 
 
 def create_players()
   players = []
-  puts "{n} players so far. Enter a player name, or type 'play':"
-  user_input = gets.chomp
-  players.push(user_input)
+  while (true)
+    puts "#{players.length} players so far. Enter a player name, or type 'play':"
+    user_input = gets.chomp
+    break if user_input == "play"
+    players <<  [name: user_input]
+  end
   puts players
 end
+
+
+
+create_players()
+# create_cards()
