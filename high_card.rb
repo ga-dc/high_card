2c63a583b78e7e2bb9ddf94dff35e59217ef634a
robert <robert.k25@gmail.com>
2016-10-26 14:50:18 -0400

    adds players and play prompt

--- high_card.rb
+++ high_card.rb
@@ -1,6 +1,6 @@
 # Use these two arrays to generate a deck of cards.
 RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
-SUITS = [ "clubs", "diamonds", "hearts", "spades"]
+SUITS = ["clubs", "diamonds", "hearts", "spades"]
 
 players = []
 
@@ -10,3 +10,15 @@ puts deck.inspect, deck.size
 def get_card_value(deck, card)
   deck.find_index(card)+1
 end
+
+puts "Welcome to High Card"
+
+loop {
+  puts "Enter a player name to add players or (play)"
+  answer = gets.chomp().downcase
+  break if answer == "play"
+  player = {name: answer.capitalize}
+  players << player
+}
+
+puts players.inspect
