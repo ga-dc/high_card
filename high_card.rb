bebe9805e59a2d79dd4abaf7265159b709ea5b48
robert <robert.k25@gmail.com>
2016-10-26 14:24:20 -0400

    adds card ranking using an arranged index

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,12 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
+RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+SUITS = [ "clubs", "diamonds", "hearts", "spades"]
 
 players = []
+
+deck = RANKS.product(SUITS)
+puts deck.inspect, deck.size
+
+def get_card_value(deck, card)
+  deck.find_index(card)+1
+end
