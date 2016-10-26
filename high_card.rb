7e72bae4f82c22b06c61898b666650763b29fd82
Diana B. Gobar <dianabgobar@gmail.com>
2016-10-26 17:51:29 -0400

    add deck and players

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,31 @@
-# Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+require "pry" #Pry doesn't work!! ah!
+
+ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ],
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
+deck = []
 players = []
+
+deck.shuffle!
+
+ranks.each_with_index do |rank, index| #aligns each with the index number
+  suits.each do |suit|
+    deck << [rank, suit, index] #move to deck array
+  end
+end
+
+#getting player name or play!
+player = "" #player unassigned
+loop do player
+  puts "#{players.length} Players so far. Enter another player name, or type 'play'"
+  player = gets.chomp
+  if player == 'play'
+    break #ends this, skips to next method
+  end
+  players << {name:player}
+end
+
+#dealing cards once they input play
+
+
+binding.pry
