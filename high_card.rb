e54977a4229659d63dfc1efaa8997dad1e7f3244
Laura Goldstein <laura.koss.goldstein@gmail.com>
2017-02-06 20:09:58 -0500

    on step 3

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,25 @@
-# Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+cards = []
+
+ranks.each do |num|
+  suits.each do |pic|
+    cards.push([num.to_s, pic])
+  end
+end
+
+cards.shuffle!
+while players.length < 3
+  puts "#{players.length} players so far. Enter a player name, or type 'play':"
+  user_input = gets.chomp.to_s
+  if user_input == "play"
+    puts "play"
+  elsif
+    players.push({name: user_input})
+    if players.length <2
+    puts "please type 'play'"
+    end
+  end
+end
