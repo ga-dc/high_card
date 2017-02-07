4b3e76d79257b2f6e27681ac9540a09f9d64ac59
Justin Shin <jjs3b@virginia.edu>
2017-02-07 18:21:30 -0500

    finished up to dealing cards, need to do a compare

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,40 @@
+require "pry"
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ,"A" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
+deck = []
+# players = []
 
-players = []
+ranks.product(suits).each do |combo|
+  deck << combo
+end
+
+class Player
+  attr_accessor :name
+  @@players = []
+
+  def initialize(name)
+    @name = name
+    @@players << self
+  end
+  def self.all
+    return @@players
+  end
+
+end
+game_start = false
+while game_start == false
+  puts "#{Player.all.length} player(s) so far. Enter a player name or type 'play':"
+  response = gets.chomp.to_s
+  if response == "play"
+    game_start = true
+  else
+    Player.new(response)
+  end
+end
+deck.shuffle!
+Player.all.each_with_index do |playa_play,i|
+  playa_play[:card] = deck[i]
+end
+
+binding.pry
