c94847ff42016aa8feefc0056101b97fd6a4dc33
Eva Lai <evalai.dc@gmail.com>
2017-02-07 17:41:09 -0500

    created deck

--- high_card.rb
+++ high_card.rb
@@ -3,3 +3,9 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+deck = []
+
+ranks.product(suits).each do |set|
+   deck << set
+end
+p deck
