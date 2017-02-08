c9ad447ed1c8c0ae3e6fd73e3fd39689ac262d37
Chris Wightman <wightman_christopher@bah.com>
2017-02-08 10:04:51 -0500

    abandoned the OOR for now. Game is shuffling and dealing. Need to add weight or value to cards in order to determine winner.

--- high_card.rb
+++ high_card.rb
@@ -1,56 +1,46 @@
 # Use these two arrays to generate a deck of cards.
-def prep_deck
+# def prep_deck
 
   ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
   suits = [ "hearts", "spades", "clubs", "diamonds" ]
-  deck = [] #within scope of prep_deck
+  deck = []
   
-    ranks.each_with_index do |r, i|
-      i = v
-      ranks.push ({
-        rank: r,
-        value: v
-      })
-    end 
+    # ranks.each do |r,v|
+    #   ranks.push ({
+    #     rank: r,
+    #     value: v
+    #   })
+    # end 
   deck = ranks.product(suits)
   deck.shuffle!
-end
+# end
 
+# $deck = prep_deck
 
+$players = []
 
+$player_count = 0
 
-players = {
-	name: ["Dealer"]
-}
-
-player_count = players[:names].length
-players[:name].each do |names|
-	player_count +=1
-end
-
-while player_count < 2 
-puts "#{player_count} players so far. Enter a player name, or type 'play':"
-
+while $player_count < 2 
+  puts "#{$player_count} players so far. Enter a player name, or type 'play':"
 	answer = gets.chomp
+	
 	if answer != "play" 
-		players[:name] << answer
-
-		if players[:name].length == 2
-
-			return deal_cards
-		end
+		$players << answer
+    $player_count += 1
 
 	elsif answer == "play"
-		puts deal_cards
+	break
 	end
+end
 
-def deal_cards
-	player_one = players[:name][0]
-	player_two = players[:name][1]
-	player_one_card = deck.pop
-	player_two_card = deck.pop
+player_one = $players[0]
+player_two = $players[1]
+player_one_card = deck.pop
+player_two_card = deck.pop
 
-	puts "#{players[:name][1] + 's card is #[player_one_card]'}"
+puts "#{$players[0]}'s card is #{player_one_card}"
+puts "#{$players[1]}'s card is #{player_two_card}"
 
 	if player_one_card > player_two_card
 		puts "'The winner is ' + player_one!"
@@ -60,7 +50,6 @@ def deal_cards
 
 	elsif player_two_card > player_one_card
 		puts "It's a tie!"
+		return deal_cards
 	end
-end
-end
 
