d31b0c040b1cd6863166bd4c9952f9c227828717
Ebenezer Williams <ebenezerw@me.com>
2017-02-08 09:42:32 -0500

    created all combination of cards and added to new deck

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,15 @@
+require "pry"
+
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+deck = []
+
+## creates all possible combo of the two arrays and shovels them in to deck
+deck << ranks.product(suits).each do |combo|
+end
+
+
+binding.pry
