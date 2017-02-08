70a2d4cfe6a684d48df2b985e2b9a9d71687b239
Savraj Singh <savrajsingh@gmail.com>
2017-02-08 09:21:27 -0500

    initial commit

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,39 @@
+require "pry"
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
-players = []
+deck = suits.product(ranks)
+
+shuffled = deck.shuffle
+
+
+
+class Player
+
+  @@players = []
+
+  def initialize(name)
+    @name = name
+    @@players << self
+  end
+
+  def self.amt_players
+    puts "#{@@players.length} people are in this game"
+    puts @@players.length
+    @@players
+  end
+
+end
+
+savraj = Player.new("Savraj Singh")
+kirat = Player.new("Kirat Singh")
+
+def play
+
+
+
+end
+
+
+binding.pry
