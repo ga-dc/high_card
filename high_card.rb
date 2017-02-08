003a89410f8203cb143844700d97e1703f5f878e
Bedig Galladian <bgalladian@gmail.com>
2017-02-07 20:12:52 -0500

    got players names in there, and able to break when player enters p

--- high_card.rb
+++ high_card.rb
@@ -4,4 +4,41 @@ suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
 
-puts deck = ranks.product(suits).shuffle
+ranks.product(suits).each_with_index do |deck|
+  puts deck = ranks.product(suits).shuffle
+end
+
+
+loop do
+  puts "#{players.length} players are currently playing.  Please type player name or type p to begin"
+  player = gets.chomp
+  if player != "p"
+    players << {name:player}
+  end
+break if player == "p"
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# original code
+# while true
+#   puts "What is player ones name?"
+#   p1_name = gets.chomp
+#   players << p1_name
+#   puts "Welcome!  What is player twos name?"
+#   p2_name = gets.chomp
+#   players << p2_name
+#
+#
+# end
