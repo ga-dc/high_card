589e0dd107c1b5ecd0987ee011e42e7e63cb9e6e
Nat Carter <n.goldman.carter@gmail.com>
2017-02-08 09:29:17 -0500

    refactored deck-making; added player-getting and dealing

--- high_card.rb
+++ high_card.rb
@@ -1,20 +1,36 @@
-require "pry"
-
 ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 suits = ["clubs", "diamonds", "hearts", "spades"]
+deck = ranks.product(suits).shuffle!
+players = []
 
-deck = []
-
-ranks.each do |rank|
-  suits.each do |suit|
-    deck.push [rank, suit]
+loop do
+  puts "#{players.length} players so far. Enter a player name, or type 'play':"
+  input = gets.chomp
+  if input != "play"
+    players << {name: input}
+  else
+    break
   end
 end
 
-deck.shuffle!
+for player in players do
+  player[:card] = deck.shift
+end
 
-players = []
+# 4. Find the highest card score dealt (Aces high).
+# 5. Find the winning player name, then print out:
+# 	* **"Winner(s): {name1, name2, …}!"**
+#
+# ### Bonus I
+#
+# Print out *one* of the following outcomes:
+#
+# * **"The winner is {name}!"**
+# * **"It's a tie between {name1, name2, …}!"**
+#
+# ### Bonus II
+#
+# Have the game redraw cards for all players in the case of a tie.
 
-binding.pry
-
-puts "End of file"
+# require "pry"
+# binding.pry
