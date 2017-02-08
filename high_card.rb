e243f71a5fc7efb19b86d491cdc9f0b4e3fbd010
Chris Wightman <wightman_christopher@bah.com>
2017-02-07 20:06:17 -0500

    Commiting before making large changes and making a class with def's

--- high_card.rb
+++ high_card.rb
@@ -6,39 +6,46 @@ deck = ranks.product(suits)
 deck.shuffle!
 
 players = {
-	name: []
+	name: ["Dealer"]
 }
 
-player_count = hash.new 0
-decks[:name].each do |names|
-	counts[names] +=1
+player_count = 0
+players[:name].each do |names|
+	player_count +=1
 end
 
-while true
-	input = gets.chomp
-	break if input.empty
-	players << input
-end
-
+while player_count < 2 
 puts "#{player_count} players so far. Enter a player name, or type 'play':"
 
-	answer = gets.chomo
+	answer = gets.chomp
 	if answer != "play" 
 		players[:name] << answer
 
 		if players[:name].length == 2
-			puts play
+
+			return deal_cards
 		end
 
 	elsif answer == "play"
-		puts play
+		puts deal_cards
 	end
 
-def play
-	player_one_card = deck.sample
-	player_two_card = deck.sample
+def deal_cards
+	player_one = players[:name][0]
+	player_two = players[:name][1]
+	player_one_card = deck.pop
+	player_two_card = deck.pop
 
-	puts "#{players:[name][1] + 's card is #[player_one_card]'}"
+	puts "#{players[:name][1] + 's card is #[player_one_card]'}"
 
-	if player_one_card < player_two_card
+	if player_one_card > player_two_card
+		puts "'The winner is ' + player_one!"
 
+	elsif player_two_card > player_one_card
+		puts "'The winner is ' + player_one!"
+
+	elsif player_two_card > player_one_card
+		puts "It's a tie!"
+	end
+end
+end
