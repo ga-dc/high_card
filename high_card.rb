8cbbf6de6ad10c77df9cb56980454a7e3b926320
Mike Nabil <maikel_nabil@yahoo.com>
2017-02-08 16:11:41 -0500

    added the bonus task for draw situations

--- high_card.rb
+++ high_card.rb
@@ -49,7 +49,41 @@ puts "Welcome to High Cards. There are #{$players.length} players so far. Player
     elsif $players[1][:card][0] > $players[0][:card][0]
       puts "#{$players[1][:name]} has a higher card. Thus he wins."
     elsif $players[0][:card][0] == $players[1][:card][0]
-      puts "Both players have equal cards. It's a draw."
+      puts "Both players have equal cards. It's a draw. We will give them a second chance"
+          for player in $players
+            player[:card] = deck.sample
+          end
+          puts "#{$players[0][:name]}'s new card is #{$players[0][:card]}"
+          puts "#{$players[1][:name]}'s new card is #{$players[1][:card]}"
+          for player in $players do
+            if $players[0][:card][0].to_s == "A"
+              $players[0][:card][0] = 14
+            elsif $players[1][:card][0].to_s == "A"
+              $players[1][:card][0] = 14
+            elsif $players[0][:card][0].to_s == "J"
+              $players[0][:card][0] = 11
+            elsif $players[1][:card][0].to_s == "J"
+              $players[1][:card][0] = 11
+            elsif $players[0][:card][0].to_s == "Q"
+              $players[0][:card][0] = 12
+            elsif $players[1][:card][0].to_s == "Q"
+              $players[1][:card][0] = 12
+            elsif $players[0][:card][0].to_s == "K"
+              $players[0][:card][0] = 13
+            elsif $players[1][:card][0].to_s == "K"
+              $players[1][:card][0] = 13
+            end
+          end
+          if $players[0][:card][0] > $players[1][:card][0]
+            puts $players[0][:card][0]
+            puts "#{$players[0][:name]} has a higher card. Thus he wins."
+          elsif $players[1][:card][0] > $players[0][:card][0]
+            puts "#{$players[1][:name]} has a higher card. Thus he wins."
+          elsif $players[0][:card][0] == $players[1][:card][0]
+            puts "Both players have equal cards. It's a draw. There is no third chance!"
+          end
+
+
     end
 
   end
