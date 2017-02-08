13b6bb370e41a221b015a02f80c52e04b750bdfd
Dan Lin <dlin2102@gmail.com>
2017-02-08 14:10:35 -0500

    question1

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,13 @@
-# Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
-players = []
+
+
+
+  ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+  suits = [ "hearts", "spades", "clubs", "diamonds" ]
+  deck_of_cards = []
+  player_names = []
+
+  deck_of_cards = (ranks).product(suits)
+  print deck_of_cards
+
+  
