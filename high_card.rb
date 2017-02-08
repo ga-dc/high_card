c8847380bc2daf87f1520ca16a675f858b60891e
Samuel Clements <sdkclements@gmail.com>
2017-02-08 13:28:11 -0500

    got basic arrary and shuffle and player creation

--- high_card.rb
+++ high_card.rb
@@ -3,3 +3,28 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+
+deck = ranks.product(suits)
+
+class Player
+	attr_accessor :name
+	def initialize(name, cards = [])
+		@name = name
+		@cards = cards
+	end
+	def add
+		players << self
+	end
+end
+
+sam = Player.new("Sam")
+
+p deck.shuffle
+# puts  "#{players.length} players so far. Enter a player name, or type 'play' "
+# user_input = gets.chomp
+# if user_input == 'play'
+# 	deck.shuffle
+# 	players.map{|x| x}
+# else Player.new(user_input)
+# end
+
