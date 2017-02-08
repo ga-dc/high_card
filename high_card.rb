b571bf42e5738cfa71c072f478490065c846810c
Erica Norby <ericanorby@gmail.com>
2017-02-07 21:02:31 -0500

    initial commit

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,25 @@
-# Use these two arrays to generate a deck of cards.
+require "pry"
+
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
+deck = ranks.product(suits).shuffle
+
 players = []
+
+player_name = ""
+
+loop do
+  puts "#{players.length} players so far. Enter a player name, or type 'play'"
+  player_name = gets.chomp
+  break if player_name == "play"
+  players << player_name
+end
+
+players.map!{|person| {name: person}}
+
+players.each_with_index do |person, i|
+  person[:card] = deck[i]
+end
+
+binding.pry
