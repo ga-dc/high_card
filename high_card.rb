5e5e742d8dec01121fb455408519a0c04a60bf9e
Mike Nabil <maikel_nabil@yahoo.com>
2017-02-07 21:15:08 -0500

    added a shuffled deck

--- high_card.rb
+++ high_card.rb
@@ -1,6 +1,15 @@
-# Use these two arrays to generate a deck of cards.
+require "pry"
+
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
+deck = []
 
-players = []
-  
+ranks.each do |rank|
+  suits.each do |suit|
+    deck.shuffle! << [rank, suit]
+  end
+end
+p deck
+
+
+binding.pry
