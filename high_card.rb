ec5f2ae0b9dbbc3bee9ffb42be1dc198a5f0a2a4
Hannah Schuster <hschuster9@gmail.com>
2017-02-08 09:26:34 -0500

    second question updated

--- high_card.rb
+++ high_card.rb
@@ -1,3 +1,4 @@
+require "pry"
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
@@ -18,10 +19,17 @@ deck.shuffle!
 
 p deck
 
-
-def prompt(n)
-  players = []
-  p "#{players.length} players so far. Enter a player name, or type 'play':"
-  players.push([name: n],)
-
+players = []
+loop do
+  name = ""
+  puts "#{players.length} players so far. Enter a player name, or type 'play':"
+  name = gets.chomp
+  break if name == "play"
+  players.push([name: name],)
 end
+
+
+
+
+binding.pry
+ puts "end of file"
