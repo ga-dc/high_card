7ad99f8365ffdadc501b85c9011b74ec216f5887
Ebenezer Williams <ebenezerw@me.com>
2017-02-08 17:42:43 -0500

    working on loop

--- high_card.rb
+++ high_card.rb
@@ -4,12 +4,20 @@ require "pry"
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
-players = []
+
 deck = []
+players = []
 
 ## creates all possible combo of the two arrays and shovels them in to deck
-deck << ranks.product(suits).each do |combo|
+deck = ranks.product(suits).shuffle.each do |combo|
 end
 
+loop do |names|
+  puts "{name.count} players so far. Enter a player name, or type 'play':"
+  name = ""
+  if condition
+
+  end
+end
 
 binding.pry
