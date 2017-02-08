456399250a9ad87113a52f89534f1723cbe71bec
jared edwards <jredwards519@gmail.com>
2017-02-08 12:40:48 -0500

    reached point of card comparison without working and printing winner

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,44 @@
+require "pry"
+
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K","A"  ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
+deck = []
+deck = ranks.product(suits);
+# deck.shuffle
+
 players = []
+
+puts "Enter a name?? (y/n)"
+ans = gets.chomp
+index = 0
+until ans == "n"
+  puts "Enter a player name"
+  players << person = {name: gets.chomp }
+  index += 1
+  puts "Would you like to enter another name? (y/n)"
+  ans = gets.chomp
+end
+
+#populate players cards
+players.each{|player| player[:card] = deck.sample}
+
+compare = []
+
+ranks.each_with_index {|rank, index| compare << index }
+
+players[0][:card].each_with_index( |rank, index| compare.each{|index| puts rank <= index}}
+
+#Need to modify this to get the index of the card of each player... (not )
+#then need to
+
+
+
+
+
+
+
+
+
+binding.pry
