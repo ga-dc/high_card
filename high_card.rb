4f7c9b9f8d9964acf1fcc5e2c0970019c826c936
Eva Lai <evalai.dc@gmail.com>
2017-02-08 00:01:56 -0500

    added players and put them in hashes

--- high_card.rb
+++ high_card.rb
@@ -4,8 +4,22 @@ suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
 deck = []
-
+# makes and shuffles deck
 ranks.product(suits).shuffle.each do |set|
    deck << set
 end
 p deck
+
+# creates players and puts them in array as hashes
+loop do
+  puts "#{players.length} players so far. Enter a player name or type 'play':"
+  input_name = gets.strip
+  # to add hashes into players array
+  player= {:name => input_name}
+  # players << player
+  break if input_name == "play"
+  players << player
+end
+p players
+
+# deals each player a card and deletes card from deck
