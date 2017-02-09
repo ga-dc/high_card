8ba2b403caf825d56b3b21ba11369790cbe8623f
Bill Bobbitt <wmbobbitt@gmail.com>
2017-02-08 20:35:37 -0500

    announces winner

--- high_card.rb
+++ high_card.rb
@@ -20,3 +20,19 @@ end
 players.each_with_index do |person, i|
   person[:card] = deck[i]
 end
+
+players.each do |person|
+  if person[:card][0] == 'A'
+    person[:card][0] = 14
+  elsif person[:card][0] == 'J'
+    person[:card][0] = 11
+  elsif person[:card][0] == 'Q'
+    person[:card][0] = 12
+  elsif person[:card][0] == 'K'
+    person[:card][0] = 13
+  end
+end
+
+winner = players.max { |a, b| a[:card][0] <=> b[:card][0]}
+
+puts "Congrats #{winner}!"
