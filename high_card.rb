ba0616dbd4ffdf7bc20b048c6776cc7db924f5a4
Natasha Clark <Natasharclark@gmail.com>
2017-02-09 08:38:23 -0500

    starting on game logic?

--- high_card.rb
+++ high_card.rb
@@ -4,27 +4,26 @@ require "pry"
 ranks = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
-players =[]
+players =[
+  
+]
 
 deck = ranks.product(suits).each do |card| "#{card[0]} #{card[1]}"
 end
 
 playing_deck = deck.shuffle
 
-loop do
-  puts "#{players.length} players so far. Enter a player name, or type 'play'"
-answer = gets.chomp
-players << answer
-    break if answer == 'play'
-  end
+def enter_players
+  puts "please enter player name"
+    answer = gets.chomp
+    if answer == "play"
+      run_game
+    else players << answer
+end
 
-players.pop
-
-def play (players)
-  @players = players
-
-# How do I get the created players into the array?? 
-
+def run_game (players)
+  players.each do |players|
+    puts " #{[:name]} draws playing_deck[0]"
 
 end
 
