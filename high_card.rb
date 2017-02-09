49a0f3e9732f8f0834ad7edc8bfafda61816d8a3
xgironx <xgironx@gmail.com>
2017-02-09 08:54:21 -0500

    trouble with deal

--- high_card.rb
+++ high_card.rb
@@ -1,76 +1,45 @@
-# 201702071755L   EL MARTES   GIRO
-# Use these two arrays to generate a deck of cards.
+              # 201702071755L   EL MARTES   GIRO
+              # Use these two arrays to generate a deck of cards.
 
 players = []
+players = ['annie', 'bobbie', 'charlie']
 deck    = []
 ranks   = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits   = [ "hearts", "spades", "clubs", "diamonds" ]
+card = []
+play = false
+deck = ranks.product(suits).shuffle
+                # p deck.shuffle
+p "our deck looks like this   ---> "
+p deck
+                # Thu Feb 09 08:28:16 ~/wdi/homework/due_201702090900L/high_card (master *) # $ ruby high_card.rb # "our deck looks like this   ---> " # [[8, "hearts"], [5, "clubs"], [6, "diamonds"], ["A", "hearts"], [8, "clubs"], [2, "hearts"], [7, "diamonds"], [7, "clubs"], [6, "spades"], [9, "spades"], [2, "diamonds"], [2, "clubs"], [9, "hearts"], [4, "hearts"], ["Q", "hearts"], [6, "hearts"], ["A", "clubs"], [8, "diamonds"], [3, "clubs"], ["J", "diamonds"], [4, "clubs"], [7, "spades"], ["J", "spades"], [4, "spades"], [10, "hearts"], ["K", "hearts"], ["Q", "clubs"], [9, "diamonds"], [6, "clubs"], ["K", "clubs"], [10, "diamonds"], [10, "spades"], ["Q", "spades"], ["Q", "diamonds"], [3, "hearts"], [3, "diamonds"], [10, "clubs"], ["J", "hearts"], [2, "spades"], [7, "hearts"], ["K", "diamonds"], ["A", "diamonds"], [3, "spades"], [5, "hearts"], [9, "clubs"], [4, "diamonds"], [5, "diamonds"], ["K", "spades"], [8, "spades"], ["J", "clubs"], ["A", "spades"], [5, "spades"]] # LETS PLAY POKER
+    puts ''
+    puts 'LETS PLAY'
+    puts "we have --->      #{players.count}    <---   players so far."
+    p players
+    puts ''
+    puts "enter a player name, or type 'play':"
+    player_or_play = gets.chomp.to_s
 
 
-deck = ranks.product(suits)
-        # [["A", "hearts"], ["A", "spades"], ["A", "clubs"], ["A", "diamonds"], [2, "hearts"], [2, "spades"], [2, "clubs"], [2, "diamonds"], [3, "hearts"], [3, "spades"], [3, "clubs"], [
+    def add_players(players, cards, index)
+      puts "we have #{players.length} players."
+      puts "enter a player name, or type 'play':"
+         user_input = gets.chomp.to_s
+      if user_input != "play"
+        players.push({name: user_input})
+        puts "meet your fellow players #{players}"
+      else
+        until index == players.length
+          players.each do |deal|
+                           deal[:card] = deck.pop
+            index += 1
+          end
+      end
+              winner = players.max_by{|max| max[:card]}
+      puts "#{winner[:name]} wins! with #{winner[:card]}"
+      # rank_cards(players)
+    end
+    end
 
-# puts "_"
-# puts "puts deck.shuffle"
-# puts "LOOKS LIKE THIS"
-# puts "SEPARATE LF CRLF FOR EACH HASH VALUE "
-# puts deck.shuffle
-
-puts "_"
-puts "p deck.shuffle"
-puts "LOOKS LIKE THIS: "
-puts "CREATES EACH deck AS ITS OWN HASH"
-puts "WITH FAT ARROW THING, WHITE TXT IN REPL THEN FAT ARROW => GREEN TXT"
-p deck.shuffle
-
-
-
-"{n} players so far. Enter a player name, or type 'play':"
-Should look like this: [{name:'Jesse'}, {name:'Jane'}]
-
-
-Arrays
-Consider:
-users = ["Alice", "Bob", "Carol"]
-What do each of the following do? users.length users.push("David") users[0] users[2] users.join(" ") users.join(", ") users.join(" and ")
-What are the two ways of adding items to the end of an array of unknown length in Ruby?
-.push and <<
-
-Hashes
-Hashes are like Javascript Object Literals, but with a somewhat different syntax.
-Consider:
-user = {
-  name: "Alice",
-  age: 30,
-  skills: ["development", "public speaking", "physics"]
-}
-
-
-# require "pry"
-# class Player
-#   def set_name_to(some_string)
-#     @name = some_string
-#   end
-#   def get_name
-#     return @name
-#   end
-#   def greet
-#     puts "Hi! My name is #{@name}!"
-#   end
-# end
-# binding.pry
-# puts "end of file"
-#
-#
-# class Player
-#
-#   def initialize(firstname, lastname)
-#     @firstname = firstname
-#     @lastname = lastname
-#   end
-#
-#   def full_name
-#     return "#{@firstname.capitalize} #{@lastname.capitalize}"
-#   end
-#
-# end
+    puts card
