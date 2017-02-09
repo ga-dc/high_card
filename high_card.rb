c1ec313b31759f242a84f4464fa20b9e1de88934
Fiona <h.natapra@gmail.com>
2017-02-08 19:52:17 -0500

    final commit

--- high_card.rb
+++ high_card.rb
@@ -1,9 +1,9 @@
 require "pry"
-
-
-
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+
+# ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+# I removed the Letters ranks for less complexity when calling the winning card
+ranks = [ 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
@@ -16,7 +16,7 @@ compare = []
     newPlayer = gets.chomp
     break if newPlayer == "play"
     players << {name: newPlayer, card: deck.pop}
-    # compare << players[:card]
+
   end
 
   players.each do |player|
@@ -24,12 +24,10 @@ compare = []
     compare << player[:card]
   end
 
-
-  ## CONTINUE NO 4 & 5
-
-
-
-
+ # Able to pull out the winning card, but not the winner's name associated with the card
+ high_card =  compare.max_by do |comp|
+   comp[0]
+  end
 
 
 binding.pry
