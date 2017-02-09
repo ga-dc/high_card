568179fb34d7bacbb4959ade378627ecc80ad293
Bill Bobbitt <wmbobbitt@gmail.com>
2017-02-08 20:09:41 -0500

    dealing works

--- high_card.rb
+++ high_card.rb
@@ -4,6 +4,19 @@ suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 deck = ranks.product(suits).shuffle!
 
-p deck
-
 players = []
+answer = 'String'
+
+while true
+  puts "There are #{players.length} players playing so far. Enter a player name, or type 'play'."
+  answer = gets.chomp.capitalize.to_s
+  players.push({name: answer})
+  if answer == 'Play'
+    players.pop
+    break
+  end
+end
+
+players.each_with_index do |person, i|
+  person[:card] = deck[i]
+end
