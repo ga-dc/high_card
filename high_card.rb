9e102181b2178bc9cf6072adccb6db5b261bbeb3
Hannah Schuster <hschuster9@gmail.com>
2017-02-08 19:31:26 -0500

    high card found

--- high_card.rb
+++ high_card.rb
@@ -1,6 +1,8 @@
 require "pry"
+
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+# ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
@@ -8,11 +10,7 @@ cards = []
 deck = []
 
 # 1 build deck and shuffle cards
-ranks.each_with_index do |rank, index|
-  suits.each do |suit|
-    deck.push( [rank, suit])
-  end
-end
+deck = ranks.product(suits)
 
 deck.shuffle!
 
@@ -20,23 +18,25 @@ deck.shuffle!
 p deck
 
 #2 prompt names and add to players array
+#3 deal players a card
 players = []
 loop do
   name = ""
   puts "#{players.length} players so far. Enter a player name, or type 'play':"
   name = gets.chomp
   break if name == "play"
-  players.push([name: name],)
-end
+  players.push({name: name, cards: deck.pop} )
 
-#3 deal players a card
-cards = players.map  do |player|
-    deck.pop
 end
 
 
-high_card = cards.max
+# #4 high card
+get_card = players.max_by do |player|
+  puts player
+  player[:cards][0]
+end
 
+puts get_card
 
 
 
