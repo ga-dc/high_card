c3e4455a6cbd86d727447091af076bde594ceb3e
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-02-08 23:30:10 -0500

    updating

--- high_card.rb
+++ high_card.rb
@@ -1,41 +1,48 @@
 require "pry"
 
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
+ranks = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K","A"]
+suits = [ "hearts", "clubs", "diamonds", "spades" ]
 
 nameofplayers = []
 
 # shuffle both arrays
   deck = ranks.product(suits)
+  deck.each { |rank,suit|}
   deck.shuffle!
   p deck
+
+# give me a player names possible solutions would be (while loop possibly)
+# ask for the player and get the player with gets
+# store player somewhere
+# print out an array of players (progress)
+# check if player is done
+# deal the cards and give each playercards
 # add values to each arrays (I need help with this part)
-
-# ask each player for their name
-nameofplayers
-puts "{n} players so far. Enter a player name or type 'play'"
-
-# if you get a name then you add it to a players arrays
-
-nameofplayers = []
-nameofplayers.push()
-
-# if you get the word play, then you deal the cards
-
-if nameofplayers = [](play)
-   puts "your turn to deal"
- else
-   puts "not your turn to deal"
+    n = 0
+while true
+    puts "#{n} players so far. Enter a player name, or type 'play':"
+    playername = gets.chomp
+    puts playername
+    break if playername.upcase == "PLAY"
+    nameofplayers.push({name:playername})
+    n +=1
+end
+dealedcards = nameofplayers.map do |player|
+  deck.pop
 end
 
+puts dealedcards
+
+
 # then give each player a card (I need help with this part)
-ranks.product(suits).find("A hearts","A spades","A clubs","A diamond") do |pick|
-  if ranks.product(pick)
-    else
-      next
-    end
-#calculate the highest card (help)
+# ranks.product(suits).find("A hearts","A spades","A clubs","A diamond") do |pick|
+#   if ranks.product(pick)
+#     else
+#       next
+#     end
+# end
+# calculate the highest card (help)
 
 binding.pry
 puts "end of file"
