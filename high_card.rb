0cfff087895bd07b51e5f1cca1949169e983c5c0
Habtamu Abere <habbuya@gmail.com>
2017-02-09 02:19:51 -0700

    needs work

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,5 @@
 # Use these two arrays to generate a deck of cards.
-rranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 
@@ -9,41 +9,62 @@ players = []
 deck = ranks.product(suits).shuffle!
 p deck
 
-cards = deck.each {|card| p card}
+#cards = deck.each {|card| p card}
 
 puts "How many Players?"
 
 
-player_num = $stdin.gets.to_i
+player_num = gets.chomp.to_i
 
 
 
  player_num.times do
  puts "Would you like to play? Enter your name:"
 
-    names = $stdin.gets.chomp
+ names = gets.chomp
 
-
-
-
- players = [:name]
-
-
-
-   players << names.to_s
-
- #  players.merge!(name:  #{names} )
-
-
+   players << {name: names}
 
 puts "hello #{names}"
 puts players
 
 end
 
+
 puts "Let's play, ready to play-yes or no:"
 
-play = $stdin.gets.chomp
-puts "Let's play, ready to play-yes or no:"
+play = gets.chomp
 
-play = $stdin.gets.chomp
+
+if play == "yes"
+
+   p players
+
+
+
+
+     winners = []
+    # new_card =[]
+
+
+    # new_card = cards.push(players[index])
+    cards = players.map do |player|
+     deck.pop
+
+     players_hand = ranks.index(player[:card])
+
+
+
+      house = players.map{|player|  players_hand}
+
+      win = house.max
+
+     if  players_hand == win
+
+       winners.push(players)
+
+     end
+   end
+      else
+        puts "it's a draw"
+      end
