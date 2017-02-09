2949ed089ed721bc433617d212f368735b3c4ddf
Bedig Galladian <bgalladian@gmail.com>
2017-02-08 20:43:39 -0500

    finally finished and got a winner

--- high_card.rb
+++ high_card.rb
@@ -1,44 +1,42 @@
-# Use these two arrays to generate a deck of cards.
+
+# require "pry"
+
+# Use these two arrays to generate a deck of cards
+
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks.push(ranks.shift)
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+deck = []
 
-ranks.product(suits).each_with_index do |deck|
-  puts deck = ranks.product(suits).shuffle
+ranks.each_with_index do |rank, index|
+suits.each do |suit|
+  deck << [rank, index, suit]
 end
+end
+
 
 
 loop do
-  puts "#{players.length} players are currently playing.  Please type player name or type p to begin"
-  player = gets.chomp
-  if player != "p"
-    players << {name:player}
-  end
-break if player == "p"
+    puts "#{players.length} players are currently playing.  Please type player name or type play to begin"
+    player = gets.chomp
+    if player != "play"
+      players << {name:player, card:deck.sample}
+    end
+  break if player == "play"
 end
 
+players.each do |player|
+  player[:card] = deck.shift
+end
+
+winning_player = players.max_by do |player|
+  player[:card][0]
+end
+
+puts winning_player
 
 
 
-
-
-
-
-
-
-
-
-
-
-# original code
-# while true
-#   puts "What is player ones name?"
-#   p1_name = gets.chomp
-#   players << p1_name
-#   puts "Welcome!  What is player twos name?"
-#   p2_name = gets.chomp
-#   players << p2_name
-#
-#
-# end
+# binding.pry
