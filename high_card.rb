d8298904c3ca13bfef0d63cb23475874122a4cda
Dan Lin <dlin2102@gmail.com>
2017-02-08 21:40:30 -0500

    commit2

--- high_card.rb
+++ high_card.rb
@@ -1,13 +1,38 @@
 
-
-
-
   ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
   suits = [ "hearts", "spades", "clubs", "diamonds" ]
   deck_of_cards = []
-  player_names = []
+  $player_names = []
 
   deck_of_cards = (ranks).product(suits)
-  print deck_of_cards
+  deck_of_cards.shuffle!
 
-  
+def get_names(names)
+puts "{player_name.length} players so far. Enter a player name, or type play"
+loop do
+  user_input = gets.chomp.to_s
+  break if user_input == "play"
+  names << user_input
+end
+play()
+end
+
+def play()
+  if $player_names.length >= 2
+    puts "deal card"
+  end
+end
+
+get_names($player_names)
+
+  ranks.each_with_index do |rank, i|
+   suits.each do |suit|
+     deck_of_cards << [i, rank, suit]
+     end
+  end
+
+winning_player = players.max_by do |player|
+  player[:card][0]
+end
+
+puts winning_player
