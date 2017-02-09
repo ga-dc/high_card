e6f062d4ce452cde723a39d173924c2e25fd4cfb
Gurpreet Gill <gill_gurpreet2@bah.com>
2017-02-09 06:14:58 -0500

    updated rb files

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,72 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
+#create a new deck function
+def deck_cards
+    ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+    suits = [ "hearts", "spades", "clubs", "diamonds" ]
+    deck = []
+    players = []
 
-players = []
+    values.each_with_index do |value, index|
+      suits.each do |suit|
+        deck.push([value,suit])
+    end
+ end
+   return deck.shuffle
+end
+
+#collect players function
+
+class Player
+  attr_accessor :name
+  attr_accessor :card
+  @@players = []
+
+  def initialize(name)
+    @name = name
+    @card = nil
+    @@players << self
+  end
+  def self.all
+    return @@players
+  end
+  def set_card(card)
+    @card = card
+  end
+  def get_card
+    return @card
+  end
+  def get_name
+    return @name
+  end
+
+#Upon *"play"*, deal each player a card.
+def deal_cards(deck)
+  hands []
+  players.each do |player|
+    hand = {
+      player: = player
+      card: = deck.shift
+    }
+      hands << hand
+    end
+    hands
+  end
+
+# Find the highest score in the game
+scores = cards.map do |card|
+  card[:score]
+end
+
+high_score = scores.max
+
+
+# Select winners of the game
+winners = []
+
+scores.each_with_index do |score, index|
+  winners.push(players[index]) if score == high_score
+end
+
+puts "Winner(s): #{winners.join(', ')}"
+puts cards
+end
