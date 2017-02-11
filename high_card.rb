98603d729f41306c5fb70e53f4981d4ad34bd4a3
Malcom Johnson <malcomjohnson@malcoms-MacBook-Pro.local>
2017-02-11 16:05:57 -0500

    first commit

--- high_card.rb
+++ high_card.rb
@@ -3,3 +3,11 @@ ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 players = []
+players = [i]
+deck = []
+players = []
+card = []
+
+break if user_imput == "play"
+players << {name:user_input}
+end
