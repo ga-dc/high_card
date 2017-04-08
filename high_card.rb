34160970ed1a2e0a72c94b59fc53d62bf3fb3fe7
Jonathan Wright <jonwright.va@gmail.com>
2017-04-08 10:20:03 -0400

    deck generated and randomized

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,14 @@
 # Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
+ranks = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
+suits = ['hearts', 'spades', 'clubs', 'diamonds']
 
-players = []
+deck_of_cards = []
+
+suits.each do |suit|
+  ranks.each do |rank|
+    deck_of_cards.push([suit, rank])
+  end
+end
+
+# players = []
+deck_of_cards.shuffle
