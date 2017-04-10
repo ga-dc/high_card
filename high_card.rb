c1ce9cb180ed421024f44fbddb4d1bc8dbed5cc8
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-04-10 14:36:29 -0400

    card shuffle

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,21 @@
 # Use these two arrays to generate a deck of cards.
+
+
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
+deck = []
 
-players = []
+suits.each do |suit|
+  ranks.each do |rank|
+    card = [ rank, suit ] #combines the two arrays into a new array
+    deck << card
+  end
+end
+
+p deck.shuffle
+# deck = cards
+# players = []
+
+# while true
+#   puts "#{players.length} players so far. Enter a player name, or type 'play:'"
+#   name = gets.chomp
