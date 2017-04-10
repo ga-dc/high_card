4f0a4924f09dd0815a318963e33d23f0d8b837ca
Caroline Chen <carolinecaroline@gmail.com>
2017-04-09 21:57:26 -0400

    create and shuffle card deck and collect array of player names

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,17 @@
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
-
+deck = ranks.product(suits)
 players = []
+
+deck.shuffle!
+
+loop do
+	puts "#{players.length} players so far.  Enter a player name, or type 'play':"
+	user_input = gets.chomp
+	break if user_input == "play"
+	players << {name: user_input}
+
+end
+
+puts players
