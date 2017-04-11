69284f5ead7e88afd23eb71d3ddab338487e3acf
Ick Hyun Kwon <kwonih1020@gmail.com>
2017-04-11 09:42:54 -0400

    need to work on part 3 4 5

--- high_card.rb
+++ high_card.rb
@@ -1,21 +1,14 @@
-# Use these two arrays to generate a deck of cards.
-
 require "pry"
 
 #1 build shullfed deck cards
-
-
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+ranks = [ 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
 deck = []
 
-ranks.each do |rank|
-  suits.each do |suit|
-    deck.push ({
-      ranks: rank,
-      suits: suit
-    })
+suits.each do |suit|
+  ranks.each do |rank|
+    deck << [suit, rank]
   end
 end
 
@@ -27,21 +20,15 @@ players = []
 
 loop do
   puts "#{players.length} so far. Enter a player name, or type 'play':"
-  player = gets.chomp
+  name = gets.chomp
 
-  break if player == "play"
-  players << {name: player}
+  break if name == "play"
+  players << name
 end
 
 #3 deal each player a card
 
-deck = deck.shuffle
-
-  cards = players.map do |player|
-    deck.pop
-  end
 
 #4 highest score
 
-
-binding.pry
+  binding.pry
