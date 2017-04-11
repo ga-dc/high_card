efa496b6d13d54b308fff2656d3acd1cca3658ae
Curtis Ostle <curtisostle@gmail.com>
2017-04-10 22:53:43 -0400

    made deck, made new player class

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,38 @@
+require "pry"
+
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
+deck = suits.product(ranks).shuffle
+
 players = []
+
+class Player
+
+  def initialize(name)
+    @name = name
+  end
+
+end
+
+amount_of_players = 0
+
+puts "enter a player name or type 'play'"
+player_input = gets.chomp
+
+  if player_input == 'play'
+
+  else
+    x = Player.new(player_input)
+    players.push(x)
+    amount_of_players += 1
+  end
+
+
+
+
+
+binding.pry
+
+puts "end of file"
