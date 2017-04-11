1ef6e0ef441f63ea7d6cbef210e763cddd7cc36e
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-04-11 18:37:21 -0400

    shuffle through deck and give cards to players

--- high_card.rb
+++ high_card.rb
@@ -1,30 +1,37 @@
+# require "pry"
+
 # Use these two arrays to generate a deck of cards.
-  ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-  suits = [ "hearts", "spades", "clubs", "diamonds" ]
-  deck = []
+ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+suits = [ "hearts", "spades", "clubs", "diamonds" ]
+deck = []
+cards_dealt = []
 
-  ranks.each_with_index do |rank, i|
-    suits.each do |suit|
-      card = {
-        rank: rank,
-        suit: suit
-      }
-      deck << card
-    end
+ranks.each_with_index do |rank, i|
+  suits.each_with_index do |suit, i|
+    deck << [rank, suit]
   end
-
-deck.shuffle
+end
 
 players = []
-
 loop do
-  puts "#{players.length} players so far. Enter a player name, or type 'play'"
+  puts "#{players.length} players so far. Enter a player name, or type 'play' to start the game"
   user_input = gets.chomp
 
   break if user_input == "play"
   players << user_input
+  puts players
 end
 
-cards = players.each do |player|
+deck = deck.shuffle
 
+## Deal cards to players
+players.each_with_index do |player, index|
+  cards_dealt << {player: player, suit: deck[index][1], rank: deck[index][0]}
 end
+
+## Who has the strongest card??
+cards_dealt.each do |cards|
+  
+end
+
+# binding.pry
