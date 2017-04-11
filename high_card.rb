2388f1b7204fffbe23a7410a38d2bd89e2c9a492
Omar Chaudhry <omarchaudhry.1991@gmail.com>
2017-04-11 18:57:58 -0400

    shuffles cards and adds players

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,28 @@
-# Use these two arrays to generate a deck of cards.
-ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
-suits = [ "hearts", "spades", "clubs", "diamonds" ]
+def deck_of_cards
+  ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
+  suits = [ "hearts", "spades", "clubs", "diamonds" ]
+  deck = []
 
+  ranks.each_with_index do |v, i|
+    suits.each do |s|
+      deck.push({
+        score: i,
+        rank: v,
+        suit: s,
+      })
+    end
+  end
+
+  return deck.shuffle
+end
+
+deck = deck_of_cards
 players = []
+
+while true
+puts "We've got #{players.length} in here. If you want in, type your name. Otherwise just type play."
+name = gets.chomp
+
+break if name == "play"
+players.push(name)
+end
