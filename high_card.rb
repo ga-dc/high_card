6670ad576e022573945e1041597259c2fd9354cd
Erin <erinedawson@gmail.com>
2017-04-12 08:58:16 -0400

    final commit

--- high_card.rb
+++ high_card.rb
@@ -14,3 +14,23 @@ ranks.each_with_index do |rank| #you need to include the index, because otherwis
 end
 puts deck.shuffle
 #deck is shuffled, but am unable to get it to print in array form [8, "hearts"].  does print that way in the repl.
+
+puts "#{players.length} players so far.  Enter a player name, or type 'play':" #players.length gives that number.
+while (player_input) !== 'play'
+player_input = gets.chomp
+      #if user types anything other than 'play', that input will be assumed to be a player name, and will therefore be pushed to the players array.
+break if player_input == 'play' #once user types 'play', all inputs prior to that are logged to players array.
+  players << player_input
+end
+
+cards = players.map do |player| #what this does is go through array players, and for each object player, removes one object from deck (this will represent a card being dealt)
+deck.pop                        #we use map rather than each, because map will return a new array (a copy of our original array, with changes made).  this new array is called cards.
+end
+
+score = cards.sort_by do |card|
+  card[:score]
+end
+
+winner = score.max
+
+puts winner
