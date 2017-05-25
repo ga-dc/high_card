b30db48c9947a32d640a84e8eb86e5cab6189a6e
Bao Cong Pham <BaoPham92@yahoo.com>
2017-05-24 23:20:21 -0400

    the high card

--- high_card.rb
+++ high_card.rb
@@ -1,5 +1,39 @@
-# Use these two arrays to generate a deck of cards.
+require "pry"
+
+# Use these two arrays to generate a deck of shuffledecks.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
-players = []
+shuffledeck = ranks.product(suits).shuffle
+$tempdeck = Array.new(shuffledeck)
+$tempdeck = $tempdeck.sample(1)
+# End of deck combination with arrays, and then shuffle.
+$players = []
+
+	$i = 0
+	$max = 2 
+
+	while $i < $max do
+		puts "#{$players} of players so far. Enter $playersname to play."
+		input = gets.chomp
+		$players.push << input
+		$i += 1
+	end
+# End of player addition for 2 players.
+
+def game($players,shuffledeck)
+	$players = $players.length
+	$players.each[$tempdeck]
+end
+
+
+
+game($players,shuffledeck)
+
+binding.pry
+
+
+
+
+
+
