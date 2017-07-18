45d53ba1b83a7845e8186b1a3f804058f8921e8e
Charles Cox <cox.charles.w@gmail.com>
2017-07-18 08:48:53 -0400

    deck shuffle

--- high_card.rb
+++ high_card.rb
@@ -2,4 +2,26 @@
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
+deck = []
+
+for r in ranks do
+  for s in suits do
+    card = [r, s]
+    deck.push(card)
+  end
+end
+
+deck = deck.shuffle
+
+player = nil
 players = []
+
+while player != 'play'
+  n = players.length
+  puts "#{n} players so far. Enter a player name, or type play:"
+  players = []
+  player = gets.chomp
+  if player != 'play'
+    players.push({name: player})
+  end
+end
