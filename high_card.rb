a53d53a3673f6d1c5b095303f385e16341634712
Charles Cox <cox.charles.w@gmail.com>
2017-07-18 09:23:19 -0400

    deal each player a card

--- high_card.rb
+++ high_card.rb
@@ -2,6 +2,8 @@
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
+
+
 deck = []
 
 for r in ranks do
@@ -19,9 +21,19 @@ players = []
 while player != 'play'
   n = players.length
   puts "#{n} players so far. Enter a player name, or type play:"
-  players = []
   player = gets.chomp
   if player != 'play'
     players.push({name: player})
   end
 end
+
+l = players.length
+hands = deck.slice(0, l)
+
+# players.each do |player|
+#   player[:card] = player.hands
+# end
+
+ranks = ranks.rotate(1)
+puts hands
+puts players
