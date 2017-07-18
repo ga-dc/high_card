fdd9e8c4d88815bee0ca787f5389d6389ae1df73
Perry Fustero <pjfust@gmail.com>
2017-07-17 20:04:57 -0400

    high card correct folder, no tie

--- high_card.rb
+++ high_card.rb
@@ -2,4 +2,30 @@
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
 
+ranks = ranks.rotate(1)
+
 players = []
+
+deck = ranks.product(suits)
+deck.shuffle!
+
+players = []
+loop do
+  print "#{players.length} players so far. Enter a player name, or type 'play':"
+  player = gets.chomp
+  break if player == "play"
+  players.push({name: player})
+end
+
+players.each_with_index do |player, i|
+  player[:card] = deck[i]
+end
+
+winner = players.reduce do |current_winner, player|
+  if ranks.index(player[:card][0]) > ranks.index(current_winner[:card][0])
+    current_winner = player
+  end
+  current_winner
+end
+
+puts winner
