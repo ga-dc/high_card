8acb6e388db8b51d7eabd3f4ae8cf981d110e25d
Rami Taha <ramisn3@gmail.com>
2017-07-18 21:17:49 -0400

    second save

--- high_card.rb
+++ high_card.rb
@@ -1,7 +1,10 @@
 # Use these two arrays to generate a deck of cards.
 ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K" ]
 suits = [ "hearts", "spades", "clubs", "diamonds" ]
+
+# Build a deck of shuffled cards.
 deck = ranks.product(suits)
 s_deck = deck.shuffle
-puts s_deck
+ s_deck.each_with_index{|value,index| puts"#{index}#{value}"}
 players = []
+# user_input = gets.chomp +"{n} players so far. Enter a player name, or type 'play'
