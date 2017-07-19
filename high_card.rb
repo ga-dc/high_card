9368551a72dc63f66949cea1a70d84428541ac1d
Rami Taha <ramisn3@gmail.com>
2017-07-18 21:54:48 -0400

    add player name

--- high_card.rb
+++ high_card.rb
@@ -5,6 +5,13 @@ suits = [ "hearts", "spades", "clubs", "diamonds" ]
 # Build a deck of shuffled cards.
 deck = ranks.product(suits)
 s_deck = deck.shuffle
- s_deck.each_with_index{|value,index| puts"#{index}#{value}"}
+ s_deck.each_with_index{|value,index| puts"#{value}#{index}"}
 players = []
 # user_input = gets.chomp +"{n} players so far. Enter a player name, or type 'play'
+puts"enter player name"
+name = gets.chomp
+def player
+for player in name do
+  players.push(name)
+end
+end 
